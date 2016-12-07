pkgw/casa

commit d8faa613c34bfabafd67766a07a4f9b22488c54d

New Development: No             JIRA Issue: CAS-9096, CAS-9100, CAS-9101         Ready for Test: Yes      Interface Changes: Yes What Interface Changed: various          Test Programs: tVisCal_GT, tDJones_GT, tPJones_GT, tKJones_CT   Put in Release Notes: No              Module(s): synthesis            Description: VI2 solve migration updates to K and D solving
git-svn-id: https://svn.cv.nrao.edu/svn/casa/trunk@39080 ac9c0da5-131f-0410-9009-a9c195f183e6

code/synthesis/CMakeLists.txt
@@ -870,9 +870,12 @@ casa_add_google_test( NOT_ON_APPLE MODULES mstransform SOURCES ../mstransform/TV
 casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tCalibraterSolve_GT.cc )
 casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tCalSolVi2Organizer_GT.cc )
 casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tRecursiveVi2LayersWithCal_GT.cc )
+casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tDJones_GT.cc )
 casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tFringeJones_GT.cc )
 casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tGlinXphJones_GT.cc )
+casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tKJones_GT.cc )
 casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tMSMetaInfoForCal_GT.cc )
+casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tPJones_GT.cc )
 casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tVisVectorJonesMueller_GT.cc )
 casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tVisCal_GT.cc )
 casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tVisCalGlobals_GT.cc )

code/synthesis/MeasurementComponents/test/VisCalTestBase_GT.h
@@ -1,0 +1,92 @@
+//# tVisCalTestBase_GT: Base class for VisCal google testing
+//# Copyright (C) 2013
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This program is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU General Public License as published by the Free
+//# Software Foundation; either version 2 of the License, or (at your option)
+//# any later version.
+//#
+//# This program is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+//# more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with this program; if not, write to the Free Software Foundation, Inc.,
+//# 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+
+#include <casa/Arrays/Array.h>
+#include <casa/Arrays/ArrayMath.h>
+#include <casa/Exceptions/Error.h>
+#include <casa/iostream.h>
+#include <casa/BasicMath/Math.h>
+#include <casa/namespace.h>
+
+#include <synthesis/MeasurementComponents/DJones.h>
+#include <synthesis/MeasurementComponents/StandardVisCal.h>
+#include <synthesis/MeasurementComponents/SolveDataBuffer.h>
+#include <synthesis/MeasurementComponents/MSMetaInfoForCal.h>
+#include <msvis/MSVis/SimpleSimVi2.h>
+#include <msvis/MSVis/VisBuffer2.h>
+
+#include <gtest/gtest.h>
+
+using namespace std;
+using namespace casa;
+using namespace casacore;
+using namespace casa::vi;
+
+// <summary>
+// Test program for KJones-related classes
+// </summary>
+
+class VisCalTestBase : public ::testing::Test {
+
+public:
+
+  VisCalTestBase(Int nFld_=1,Int nScan_=1,Int nSpw_=1,
+		 Int nAnt_=4,Int nCorr_=4,Int nChan_=8,
+		 Int nTime_=1,Bool doParang=false) :
+    nFld(nFld_),nScan(nScan_),nSpw(nSpw_),
+    nAnt(nAnt_),nCorr(nCorr_),nChan(nChan_),nTime(nTime_),
+    ssvp(nFld,nScan,nSpw,nAnt,nCorr,
+	 Vector<Int>(nFld,nTime),
+	 Vector<Int>(nSpw,nChan),Complex(1.0),
+	 doParang),  // turns on poln
+    ssf(ssvp),
+    vi2(Vector<ViiLayerFactory*>(1,&ssf)),
+    vb2(vi2.getImpl()->getVisBuffer()),
+    msmc(ssvp)
+  {}
+  
+  void summary(String name) {
+    cout << name << ":" << endl
+	 << " nFld =" << nFld << endl
+	 << " nScan=" << nScan << endl
+	 << " nAnt =" << nAnt << endl
+	 << " nSpw =" << nSpw << endl
+	 << " nChan=" << nChan << endl
+	 << " nCorr=" << nCorr << endl
+	 << " nTime=" << nTime << endl;
+  }
+
+  Int nFld,nScan,nSpw,nAnt,nCorr,nChan,nTime;
+
+  SimpleSimVi2Parameters ssvp;
+  SimpleSimVi2LayerFactory ssf;
+  VisibilityIterator2 vi2;
+  VisBuffer2* vb2;
+
+  MSMetaInfoForCal msmc;
+
+};
+

commit 40d244ad3d10602e9624ad2a055c5960550eb170

New Development: No             JIRA Issue: CAS-9096, CAS-9100, CAS-9101         Ready for Test: Yes      Interface Changes: No What Interface Changed:          Test Programs: tVisCal_GT, tDJones_GT, tPJones_GT, tKJones_CT   Put in Release Notes: No              Module(s): synthesis            Description: I think these tests---broken in r39080---will 	   		now work correctly, so re-enabling them.

code/synthesis/CMakeLists.txt
@@ -870,14 +870,14 @@ casa_add_google_test( NOT_ON_APPLE MODULES mstransform SOURCES ../mstransform/TV
 casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tCalibraterSolve_GT.cc )
 casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tCalSolVi2Organizer_GT.cc )
 casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tRecursiveVi2LayersWithCal_GT.cc )
-#casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tDJones_GT.cc )
+casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tDJones_GT.cc )
 casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tFringeJones_GT.cc )
 casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tGlinXphJones_GT.cc )
-#casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tKJones_GT.cc )
+casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tKJones_GT.cc )
 casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tMSMetaInfoForCal_GT.cc )
-#casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tPJones_GT.cc )
+casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tPJones_GT.cc )
 casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tVisVectorJonesMueller_GT.cc )
-#casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tVisCal_GT.cc )
+casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tVisCal_GT.cc )
 casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tVisCalGlobals_GT.cc )
 casa_add_google_test( MODULES synthesis SOURCES MeasurementComponents/test/tSolveDataBuffer_GT.cc )
 

code/synthesis/MeasurementComponents/test/VisCalTestBase_GT.h
@@ -24,51 +24,44 @@
 //#                        Charlottesville, VA 22903-2475 USA
 //#
 
+#ifndef SYNTHESIS_MC_TEST_VISCALTESTBASE_H
+#define SYNTHESIS_MC_TEST_VISCALTESTBASE_H
+
 #include <casa/Arrays/Array.h>
-#include <casa/Arrays/ArrayMath.h>
-#include <casa/Exceptions/Error.h>
+//#include <casa/Arrays/ArrayMath.h>
 #include <casa/iostream.h>
 #include <casa/BasicMath/Math.h>
-#include <casa/namespace.h>
 
-#include <synthesis/MeasurementComponents/DJones.h>
-#include <synthesis/MeasurementComponents/StandardVisCal.h>
-#include <synthesis/MeasurementComponents/SolveDataBuffer.h>
 #include <synthesis/MeasurementComponents/MSMetaInfoForCal.h>
 #include <msvis/MSVis/SimpleSimVi2.h>
 #include <msvis/MSVis/VisBuffer2.h>
 
 #include <gtest/gtest.h>
 
-using namespace std;
-using namespace casa;
-using namespace casacore;
-using namespace casa::vi;
-
 // <summary>
-// Test program for KJones-related classes
+// Base class for VisCal GoogleTesting
 // </summary>
 
 class VisCalTestBase : public ::testing::Test {
 
 public:
 
-  VisCalTestBase(Int nFld_=1,Int nScan_=1,Int nSpw_=1,
-		 Int nAnt_=4,Int nCorr_=4,Int nChan_=8,
-		 Int nTime_=1,Bool doParang=false) :
+ VisCalTestBase(int nFld_=1,int nScan_=1,int nSpw_=1,
+		int nAnt_=4,int nCorr_=4,int nChan_=8,
+		int nTime_=1,bool doParang=false) :
     nFld(nFld_),nScan(nScan_),nSpw(nSpw_),
     nAnt(nAnt_),nCorr(nCorr_),nChan(nChan_),nTime(nTime_),
     ssvp(nFld,nScan,nSpw,nAnt,nCorr,
-	 Vector<Int>(nFld,nTime),
-	 Vector<Int>(nSpw,nChan),Complex(1.0),
+	 casacore::Vector<int>(nFld,nTime),
+	 casacore::Vector<int>(nSpw,nChan),casacore::Complex(1.0),
 	 doParang),  // turns on poln
     ssf(ssvp),
-    vi2(Vector<ViiLayerFactory*>(1,&ssf)),
+    vi2(casacore::Vector<casa::vi::ViiLayerFactory*>(1,&ssf)),
     vb2(vi2.getImpl()->getVisBuffer()),
     msmc(ssvp)
   {}
   
-  void summary(String name) {
+  void summary(casacore::String name) {
     cout << name << ":" << endl
 	 << " nFld =" << nFld << endl
 	 << " nScan=" << nScan << endl
@@ -79,14 +72,15 @@ public:
 	 << " nTime=" << nTime << endl;
   }
 
-  Int nFld,nScan,nSpw,nAnt,nCorr,nChan,nTime;
+  int nFld,nScan,nSpw,nAnt,nCorr,nChan,nTime;
 
-  SimpleSimVi2Parameters ssvp;
-  SimpleSimVi2LayerFactory ssf;
-  VisibilityIterator2 vi2;
-  VisBuffer2* vb2;
+  casa::vi::SimpleSimVi2Parameters ssvp;
+  casa::vi::SimpleSimVi2LayerFactory ssf;
+  casa::vi::VisibilityIterator2 vi2;
+  casa::vi::VisBuffer2* vb2;
 
-  MSMetaInfoForCal msmc;
+  casa::MSMetaInfoForCal msmc;
 
 };
 
+#endif  // SYNTHESIS_MC_TEST_VISCALTESTBASE_H

code/synthesis/MeasurementComponents/test/tDJones_GT.cc
@@ -29,14 +29,9 @@
 #include <casa/Exceptions/Error.h>
 #include <casa/iostream.h>
 #include <casa/BasicMath/Math.h>
-#include <casa/namespace.h>
 
 #include <synthesis/MeasurementComponents/DJones.h>
 #include <synthesis/MeasurementComponents/StandardVisCal.h>
-//#include <synthesis/MeasurementComponents/SolveDataBuffer.h>
-//#include <synthesis/MeasurementComponents/MSMetaInfoForCal.h>
-//#include <msvis/MSVis/SimpleSimVi2.h>
-//#include <msvis/MSVis/VisBuffer2.h>
 
 #include <gtest/gtest.h>
 

code/synthesis/MeasurementComponents/test/tKJones_GT.cc
@@ -29,7 +29,6 @@
 #include <casa/Exceptions/Error.h>
 #include <casa/iostream.h>
 #include <casa/BasicMath/Math.h>
-#include <casa/namespace.h>
 
 #include <synthesis/MeasurementComponents/KJones.h>
 #include <synthesis/MeasurementComponents/SolveDataBuffer.h>

code/synthesis/MeasurementComponents/test/tPJones_GT.cc
@@ -29,15 +29,21 @@
 #include <casa/Exceptions/Error.h>
 #include <casa/iostream.h>
 #include <casa/BasicMath/Math.h>
-#include <casa/namespace.h>
 
 #include <synthesis/MeasurementComponents/StandardVisCal.h>
-//#include <synthesis/MeasurementComponents/SolveDataBuffer.h>
-//#include <synthesis/MeasurementComponents/MSMetaInfoForCal.h>
-//#include <msvis/MSVis/SimpleSimVi2.h>
-//#include <msvis/MSVis/VisBuffer2.h>
+#include <synthesis/MeasurementComponents/SolveDataBuffer.h>
+#include <synthesis/MeasurementComponents/MSMetaInfoForCal.h>
+#include <msvis/MSVis/SimpleSimVi2.h>
+#include <msvis/MSVis/VisBuffer2.h>
+
+#include <gtest/gtest.h>
 
 #include "VisCalTestBase_GT.h"
+
+using namespace std;
+using namespace casa;
+using namespace casacore;
+using namespace casa::vi;
 
 // <summary>
 // Test program for PJones
@@ -82,12 +88,12 @@ TEST_F(PJonesTest, PJonesApplyTest) {
   ASSERT_EQ(VisCal::P,Papp.type());
   ASSERT_EQ(String("P Jones"),Papp.typeName());
   //  ASSERT_EQ(1,Papp.nPar());  // not really used by PJones?
-  ASSERT_EQ(false,Papp.freqDepPar());
-  ASSERT_EQ(false,Papp.freqDepMat());
-  ASSERT_EQ(false,Papp.freqDepCalWt());
-  ASSERT_EQ(false,Papp.timeDepMat());
-  ASSERT_EQ(true,Papp.isApplied());
-  ASSERT_EQ(false,Papp.isSolvable());
+  ASSERT_FALSE(Papp.freqDepPar());
+  ASSERT_FALSE(Papp.freqDepMat());
+  ASSERT_FALSE(Papp.freqDepCalWt());
+  ASSERT_FALSE(Papp.timeDepMat());
+  ASSERT_TRUE(Papp.isApplied());
+  ASSERT_FALSE(Papp.isSolvable());
 
   for (vi2.originChunks();vi2.moreChunks();vi2.nextChunk()) {
     for (vi2.origin();vi2.more();vi2.next()) {

code/synthesis/MeasurementComponents/test/tVisCal_GT.cc
@@ -93,12 +93,12 @@ TEST_F(VisCalTest, GJonesApplyState) {
   ASSERT_EQ(VisCal::G,G->type());
   ASSERT_EQ(String("G Jones"),G->typeName());
   ASSERT_EQ(2,G->nPar());
-  ASSERT_EQ(false,G->freqDepPar());
-  ASSERT_EQ(false,G->freqDepMat());
-  ASSERT_EQ(false,G->freqDepCalWt());
-  ASSERT_EQ(false,G->timeDepMat());
-  ASSERT_EQ(true,G->isApplied());
-  ASSERT_EQ(true,G->isSolvable());
+  ASSERT_FALSE(G->freqDepPar());
+  ASSERT_FALSE(G->freqDepMat());
+  ASSERT_FALSE(G->freqDepCalWt());
+  ASSERT_FALSE(G->timeDepMat());
+  ASSERT_TRUE(G->isApplied());
+  ASSERT_TRUE(G->isSolvable());
 
   /*
   IPosition sh(3,2,1,nAnt);  // nChan=1 for G
@@ -138,13 +138,13 @@ TEST_F(VisCalTest, GJonesSolveState) {
   ASSERT_EQ(VisCal::G,G->type());
   ASSERT_EQ(String("G Jones"),G->typeName());
   ASSERT_EQ(2,G->nPar());
-  ASSERT_EQ(false,G->freqDepPar());
-  ASSERT_EQ(false,G->freqDepMat());
-  ASSERT_EQ(false,G->freqDepCalWt());
-  ASSERT_EQ(false,G->timeDepMat());
-  ASSERT_EQ(false,G->isApplied());
-  ASSERT_EQ(true,G->isSolved());
-  ASSERT_EQ(true,G->isSolvable());
+  ASSERT_FALSE(G->freqDepPar());
+  ASSERT_FALSE(G->freqDepMat());
+  ASSERT_FALSE(G->freqDepCalWt());
+  ASSERT_FALSE(G->timeDepMat());
+  ASSERT_FALSE(G->isApplied());
+  ASSERT_TRUE(G->isSolved());
+  ASSERT_TRUE(G->isSolvable());
   
   ASSERT_EQ(caltablename,G->calTableName());
   ASSERT_EQ(solint,G->solint());
@@ -174,12 +174,12 @@ TEST_F(VisCalTest, BJonesApplyState) {
   ASSERT_EQ(VisCal::B,B->type());
   ASSERT_EQ(String("B Jones"),B->typeName());
   ASSERT_EQ(2,B->nPar());
-  ASSERT_EQ(true,B->freqDepPar());
-  ASSERT_EQ(true,B->freqDepMat());
-  ASSERT_EQ(false,B->freqDepCalWt());
-  ASSERT_EQ(false,B->timeDepMat());
-  ASSERT_EQ(true,B->isApplied());
-  ASSERT_EQ(true,B->isSolvable());
+  ASSERT_TRUE(B->freqDepPar());
+  ASSERT_TRUE(B->freqDepMat());
+  ASSERT_FALSE(B->freqDepCalWt());
+  ASSERT_FALSE(B->timeDepMat());
+  ASSERT_TRUE(B->isApplied());
+  ASSERT_TRUE(B->isSolvable());
 
   delete B;
 }
@@ -209,13 +209,13 @@ TEST_F(VisCalTest, BJonesSolveState) {
   ASSERT_EQ(VisCal::B,B->type());
   ASSERT_EQ(String("B Jones"),B->typeName());
   ASSERT_EQ(2,B->nPar());
-  ASSERT_EQ(true,B->freqDepPar());
-  ASSERT_EQ(true,B->freqDepMat());
-  ASSERT_EQ(false,B->freqDepCalWt());
-  ASSERT_EQ(false,B->timeDepMat());
-  ASSERT_EQ(false,B->isApplied());
-  ASSERT_EQ(true,B->isSolved());
-  ASSERT_EQ(true,B->isSolvable());
+  ASSERT_TRUE(B->freqDepPar());
+  ASSERT_TRUE(B->freqDepMat());
+  ASSERT_FALSE(B->freqDepCalWt());
+  ASSERT_FALSE(B->timeDepMat());
+  ASSERT_FALSE(B->isApplied());
+  ASSERT_TRUE(B->isSolved());
+  ASSERT_TRUE(B->isSolvable());
   
   ASSERT_EQ(caltablename,B->calTableName());
   ASSERT_EQ(solint,B->solint());
@@ -246,12 +246,12 @@ TEST_F(VisCalTest, TJonesApplyState) {
   ASSERT_EQ(VisCal::T,T->type());
   ASSERT_EQ(String("T Jones"),T->typeName());
   ASSERT_EQ(1,T->nPar());
-  ASSERT_EQ(false,T->freqDepPar());
-  ASSERT_EQ(false,T->freqDepMat());
-  ASSERT_EQ(false,T->freqDepCalWt());
-  ASSERT_EQ(false,T->timeDepMat());
-  ASSERT_EQ(true,T->isApplied());
-  ASSERT_EQ(true,T->isSolvable());
+  ASSERT_FALSE(T->freqDepPar());
+  ASSERT_FALSE(T->freqDepMat());
+  ASSERT_FALSE(T->freqDepCalWt());
+  ASSERT_FALSE(T->timeDepMat());
+  ASSERT_TRUE(T->isApplied());
+  ASSERT_TRUE(T->isSolvable());
 
   delete T;
 }
@@ -279,13 +279,13 @@ TEST_F(VisCalTest, TJonesSolveState) {
   ASSERT_EQ(VisCal::T,T->type());
   ASSERT_EQ(String("T Jones"),T->typeName());
   ASSERT_EQ(1,T->nPar());
-  ASSERT_EQ(false,T->freqDepPar());
-  ASSERT_EQ(false,T->freqDepMat());
-  ASSERT_EQ(false,T->freqDepCalWt());
-  ASSERT_EQ(false,T->timeDepMat());
-  ASSERT_EQ(false,T->isApplied());
-  ASSERT_EQ(true,T->isSolvable());
-  ASSERT_EQ(true,T->isSolved());
+  ASSERT_FALSE(T->freqDepPar());
+  ASSERT_FALSE(T->freqDepMat());
+  ASSERT_FALSE(T->freqDepCalWt());
+  ASSERT_FALSE(T->timeDepMat());
+  ASSERT_FALSE(T->isApplied());
+  ASSERT_TRUE(T->isSolvable());
+  ASSERT_TRUE(T->isSolved());
 
   ASSERT_EQ(caltablename,T->calTableName());
   ASSERT_EQ(solint,T->solint());
@@ -316,12 +316,12 @@ TEST_F(VisCalTest, DJonesApplyState) {
   ASSERT_EQ(VisCal::D,D->type());
   ASSERT_EQ(String("Dgen Jones"),D->typeName());
   ASSERT_EQ(2,D->nPar());
-  ASSERT_EQ(false,D->freqDepPar());
-  ASSERT_EQ(false,D->freqDepMat());
-  ASSERT_EQ(false,D->freqDepCalWt());
-  ASSERT_EQ(false,D->timeDepMat());
-  ASSERT_EQ(true,D->isApplied());
-  ASSERT_EQ(true,D->isSolvable());
+  ASSERT_FALSE(D->freqDepPar());
+  ASSERT_FALSE(D->freqDepMat());
+  ASSERT_FALSE(D->freqDepCalWt());
+  ASSERT_FALSE(D->timeDepMat());
+  ASSERT_TRUE(D->isApplied());
+  ASSERT_TRUE(D->isSolvable());
 
   delete D;
 }
@@ -350,13 +350,13 @@ TEST_F(VisCalTest, DJonesSolveState) {
   ASSERT_EQ(VisCal::D,D->type());
   ASSERT_EQ(String("Dgen Jones"),D->typeName());
   ASSERT_EQ(2,D->nPar());
-  ASSERT_EQ(false,D->freqDepPar());
-  ASSERT_EQ(false,D->freqDepMat());
-  ASSERT_EQ(false,D->freqDepCalWt());
-  ASSERT_EQ(false,D->timeDepMat());
-  ASSERT_EQ(false,D->isApplied());
-  ASSERT_EQ(true,D->isSolvable());
-  ASSERT_EQ(true,D->isSolved());
+  ASSERT_FALSE(D->freqDepPar());
+  ASSERT_FALSE(D->freqDepMat());
+  ASSERT_FALSE(D->freqDepCalWt());
+  ASSERT_FALSE(D->timeDepMat());
+  ASSERT_FALSE(D->isApplied());
+  ASSERT_TRUE(D->isSolvable());
+  ASSERT_TRUE(D->isSolved());
 
   ASSERT_EQ(caltablename,D->calTableName());
   ASSERT_EQ(solint,D->solint());
@@ -387,12 +387,12 @@ TEST_F(VisCalTest, KJonesApplyState) {
   ASSERT_EQ(VisCal::K,K->type());
   ASSERT_EQ(String("K Jones"),K->typeName());
   ASSERT_EQ(2,K->nPar());
-  ASSERT_EQ(false,K->freqDepPar());
-  ASSERT_EQ(true,K->freqDepMat());
-  ASSERT_EQ(false,K->freqDepCalWt());
-  ASSERT_EQ(false,K->timeDepMat());
-  ASSERT_EQ(true,K->isApplied());
-  ASSERT_EQ(true,K->isSolvable());
+  ASSERT_FALSE(K->freqDepPar());
+  ASSERT_TRUE(K->freqDepMat());
+  ASSERT_FALSE(K->freqDepCalWt());
+  ASSERT_FALSE(K->timeDepMat());
+  ASSERT_TRUE(K->isApplied());
+  ASSERT_TRUE(K->isSolvable());
 
   delete K;
 }
@@ -421,13 +421,13 @@ TEST_F(VisCalTest, KJonesSolveState) {
   ASSERT_EQ(VisCal::K,K->type());
   ASSERT_EQ(String("K Jones"),K->typeName());
   ASSERT_EQ(2,K->nPar());
-  ASSERT_EQ(false,K->freqDepPar());
-  ASSERT_EQ(true,K->freqDepMat());
-  ASSERT_EQ(false,K->freqDepCalWt());
-  ASSERT_EQ(false,K->timeDepMat());
-  ASSERT_EQ(false,K->isApplied());
-  ASSERT_EQ(true,K->isSolvable());
-  ASSERT_EQ(true,K->isSolved());
+  ASSERT_FALSE(K->freqDepPar());
+  ASSERT_TRUE(K->freqDepMat());
+  ASSERT_FALSE(K->freqDepCalWt());
+  ASSERT_FALSE(K->timeDepMat());
+  ASSERT_FALSE(K->isApplied());
+  ASSERT_TRUE(K->isSolvable());
+  ASSERT_TRUE(K->isSolved());
 
   ASSERT_EQ(caltablename,K->calTableName());
   ASSERT_EQ(solint,K->solint());