53e3f6b59dda15574a33c725b09f8b6dacbbd1f3
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2017-03-08 11:17:17 +0900

    Explicit instantiation of importnro implementation.

--- code/singledishfiller/Filler/SingleDishMSFiller2.cc
+++ code/singledishfiller/Filler/SingleDishMSFiller2.cc
@@ -8,10 +8,12 @@
 #include <singledishfiller/Filler/SingleDishMSFiller.h>
 
 #include <singledishfiller/Filler/Scantable2MSReader.h>
+#include <singledishfiller/Filler/NRO2MSReader.h>
 
 using namespace casacore;
 namespace casa { //# NAMESPACE CASA - BEGIN
 
 template class SingleDishMSFiller<Scantable2MSReader>;
+template class SingleDishMSFiller<NRO2MSReader>;
 
 } //# NAMESPACE CASA - END
