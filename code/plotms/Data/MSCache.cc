127ed8a810b93f052843717962baa0cbfe6342a5
Pam Ford <pford@nrao.edu>
2017-07-27 10:51:23 -0600

    Changed cache error msg to avoid redundancy

--- code/plotms/Data/MSCache.cc
+++ code/plotms/Data/MSCache.cc
@@ -712,7 +712,7 @@ void MSCache::trapExcessVolume(map<PMS::Axis,Bool> pendingLoadAxes) {
 		logLoad(log.getMesg());
 		deleteVm();
 		stringstream ss;
-		ss << "Insufficient memory." << endl
+        ss << "Estimated cache exceeds limits." << endl
            << "Please try using data selection, averaging," << endl
            << "checking 'Reload' (to clear unneeded cache items)," << endl
 		   << "or letting other memory-intensive processes finish.";
