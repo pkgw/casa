04d4ab3c907e9955981b7e79d0b252f1a5ebb0d1
Peter Williams <peter@newton.cx>
2017-08-09 14:54:40 -0400

    code/casadbus/utilities/DBusBase.h: "fix" for casa vs casacore namespace choice

--- code/casadbus/utilities/DBusBase.h
+++ code/casadbus/utilities/DBusBase.h
@@ -8,10 +8,7 @@
 #include <dbus-c++/dbus.h>
 #endif
 
-namespace casacore{
-
-  class Record;
-}
+#include <casa/Containers/Record.h>
 
 namespace casa {
 
@@ -25,8 +22,8 @@ class DBusThreadedBase
   void stopService();
 
   /* Methods to make translating to/from the map easy */
-  static std::map<std::string,DBus::Variant> fromRecord(casacore::Record);
-  static casacore::Record toRecord(std::map<std::string,DBus::Variant>);
+  static std::map<std::string,DBus::Variant> fromRecord(casa::Record);
+  static casa::Record toRecord(std::map<std::string,DBus::Variant>);
 
  private:
   
