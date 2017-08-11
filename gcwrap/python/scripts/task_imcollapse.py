a0ccdad7d3282426b33c2061378e90e8c9437233
David Mehringer <dmehring@nrao.edu>
2017-08-11 10:18:53 -0400

    AS-10526 enable imcollapse to write to history of created image

--- gcwrap/python/scripts/task_imcollapse.py
+++ gcwrap/python/scripts/task_imcollapse.py
@@ -65,6 +65,7 @@
 
 ###########################################################################
 from taskinit import *
+from ialib import write_image_history
 
 def imcollapse(
     imagename=None, function=None, axes=None, outfile=None, box=None,
@@ -75,19 +76,31 @@ def imcollapse(
     try :
         if (len(outfile) == 0):
             raise Exception, "oufile must be specified" 
-        myia = iatool() 
+        myia = iatool()
+        myia.dohistory(False)
         if (not myia.open(imagename)):
             raise Exception, "Cannot create image analysis tool using " + imagename
-        ia_tool = myia.collapse(
+        outia = myia.collapse(
             function, axes, outfile, region, box, chans,
             stokes, mask, overwrite, stretch
         )
-        ia_tool.done()
+        try:
+            param_names = imcollapse.func_code.co_varnames[:imcollapse.func_code.co_argcount]
+            param_vals = [eval(p) for p in param_names]   
+            write_image_history(
+                outia, sys._getframe().f_code.co_name,
+                param_names, param_vals, casalog
+            )
+        except Exception, instance:
+            casalog.post("*** Error \'%s\' updating HISTORY" % (instance), 'WARN')
+        outia.done()
         return True
     except Exception, instance:
         casalog.post( str( '*** Error ***') + str(instance), 'SEVERE')
         raise
     finally:
-        if (myia):
+        if myia:
             myia.done()
+        if outia:
+            outia.done()
 
