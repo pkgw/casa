bfda6c6d1e7c27bce12e4d6358491e8edec09d12
David Mehringer <dmehring@nrao.edu>
2017-09-28 13:48:47 -0400

    reformat change tabs to spaces

--- code/imageanalysis/ImageAnalysis/ImageMaskedPixelReplacer.h
+++ code/imageanalysis/ImageAnalysis/ImageMaskedPixelReplacer.h
@@ -7,65 +7,65 @@
 namespace casa {
 
 template <class T>  class ImageMaskedPixelReplacer : public ImageTask<T> {
-	// <summary>
-	// Top level interface for replacing masked pixel values of an image
-	// </summary>
+    // <summary>
+    // Top level interface for replacing masked pixel values of an image
+    // </summary>
 
-	// <reviewed reviewer="" date="" tests="" demos="">
-	// </reviewed>
+    // <reviewed reviewer="" date="" tests="" demos="">
+    // </reviewed>
 
-	// <prerequisite>
-	// </prerequisite>
+    // <prerequisite>
+    // </prerequisite>
 
-	// <etymology>
-	// Replace masked pixel values of an image
-	// </etymology>
+    // <etymology>
+    // Replace masked pixel values of an image
+    // </etymology>
 
-	// <synopsis>
-	// Top level interface for replacing masked pixel values of an image
-	// </synopsis>
+    // <synopsis>
+    // Top level interface for replacing masked pixel values of an image
+    // </synopsis>
 
 public:
 
     ImageMaskedPixelReplacer() = delete;
 
-	// The total pixel mask is calculated by a logical OR of the existing default
-	// image pixel mask when the mask specified by <src>mask</src>. Pixel replacement
-	// is done in place; ie on the image passed to the constructor. No copy of that image
-	// is made.
-	ImageMaskedPixelReplacer(
-		const SPIIT image,
-		const casacore::Record *const &region = nullptr,
-		const casacore::String& maskInp = ""
-	);
+    // The total pixel mask is calculated by a logical OR of the existing default
+    // image pixel mask when the mask specified by <src>mask</src>. Pixel replacement
+    // is done in place; ie on the image passed to the constructor. No copy of that image
+    // is made.
+    ImageMaskedPixelReplacer(
+        const SPIIT image,
+        const casacore::Record *const &region = nullptr,
+        const casacore::String& maskInp = ""
+    );
 
-	~ImageMaskedPixelReplacer() {}
+    ~ImageMaskedPixelReplacer() {}
 
-	// Perform the substitution. expr is the LEL expression to use for the new pixel values.
-	// If updateMask is true, the false mask values will be changed to true.
-	void replace(const casacore::String& expr, casacore::Bool updateMask, casacore::Bool verbose);
+    // Perform the substitution. expr is the LEL expression to use for the new pixel values.
+    // If updateMask is true, the false mask values will be changed to true.
+    void replace(const casacore::String& expr, casacore::Bool updateMask, casacore::Bool verbose);
 
-	casacore::String getClass() const;
+    casacore::String getClass() const;
 
 protected:
-	inline  CasacRegionManager::StokesControl _getStokesControl() const {
-		return CasacRegionManager::USE_ALL_STOKES;
-	}
+    inline  CasacRegionManager::StokesControl _getStokesControl() const {
+        return CasacRegionManager::USE_ALL_STOKES;
+    }
 
-	inline vector<casacore::Coordinate::Type> _getNecessaryCoordinates() const {
-		return vector<casacore::Coordinate::Type>(0);
- 	}
+    inline vector<casacore::Coordinate::Type> _getNecessaryCoordinates() const {
+        return vector<casacore::Coordinate::Type>(0);
+     }
 
 private:
-	static const casacore::String _class;
+    static const casacore::String _class;
 
-	// This class holds a writable image object to write pixel value
-	// changes to.
-	const SPIIT _image;
+    // This class holds a writable image object to write pixel value
+    // changes to.
+    const SPIIT _image;
 
-	static void _makeRegionBlock(
-    	casacore::PtrBlock<const casacore::ImageRegion*>& imageRegions,
-    	const casacore::Record& regions
+    static void _makeRegionBlock(
+        casacore::PtrBlock<const casacore::ImageRegion*>& imageRegions,
+        const casacore::Record& regions
     );
 };
 }
