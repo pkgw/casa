0124661558004281cd5a23d3ca37e1f27572bb2d
Cesar Enrique Garcia Dabo <cgarcia@eso.org>
2017-11-27 18:12:51 +0100

    New unit test that access the data/data corrected cubes
    This unit test just creates a synthethic MS and traverses it, accessing
    the visCubeCorrected(). It will be the basis of more complex tests
    that perform swapping of the data columns.
    
    Fixes partially CAS-10458

--- code/msvis/MSVis/test/tViiLayerFactory_GT.cc
+++ code/msvis/MSVis/test/tViiLayerFactory_GT.cc
@@ -25,6 +25,9 @@
 //#
 //# $Id$
 
+#define _POSIX_C_SOURCE 200809L //For mkdtemp(), stpcpy(), nftw()
+
+#include <ftw.h>
 #include <casa/aips.h>
 #include <casa/Exceptions/Error.h>
 #include <casacore/casa/OS/EnvVar.h>
@@ -34,8 +37,10 @@
 #include <msvis/MSVis/IteratingParameters.h>
 #include <msvis/MSVis/ViiLayerFactory.h>
 #include <msvis/MSVis/LayeredVi2Factory.h>
+#include <msvis/MSVis/TransformingVi2.h>
 #include <msvis/MSVis/SimpleSimVi2.h>
 #include <msvis/MSVis/VisBuffer2.h>
+#include <msvis/MSVis/test/MsFactory.h>
 #include <casa/iomanip.h>
 #include <gtest/gtest.h>
 
@@ -43,12 +48,24 @@ using namespace std;
 using namespace casa;
 using namespace casacore;
 using namespace casa::vi;
+using namespace casa::vi::test;
+
+int removeFile(const char *fpath, const struct stat *sb, int typeflag, 
+               struct FTW* ftwbuf);
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
 
+int removeFile(const char *fpath, const struct stat *sb, int typeflag, 
+               struct FTW* ftwbuf)
+{
+  int rv = remove(fpath);
+  if(rv)
+    perror(fpath);
+  return rv;
+}
  
 TEST( ViiLayerFactoryTest , ViiLayerFactoryBasicTest ) {
  
@@ -139,4 +156,182 @@ TEST( ViiLayerFactoryTest , ViiLayerFactoryRealDataBasicTest ) {
   ASSERT_EQ(32,chunk);
   ASSERT_EQ(2864,niter);
 }
+
+class DataSwappingTVI : public TransformingVi2
+{
+public:
+  
+  DataSwappingTVI(ViImplementation2 * inputVii) :
+    TransformingVi2 (inputVii)
+  {
+    setVisBuffer(createAttachedVisBuffer (VbRekeyable));
+  }
+
+  virtual void visibilityCorrected (casacore::Cube<casacore::Complex> & vis) const
+  {
+    ViImplementation2* vii = getVii();
+    VisBuffer2* vb = vii->getVisBuffer();
+    vis = vb->visCube();
+    return;
+  }
+  
+  void origin()
+  {
+    // Drive underlying ViImplementation2
+    getVii()->origin();
+
+    // Synchronize own VisBuffer
+    configureNewSubchunk();
+
+    return;
+  }
+
+  void next()
+  {
+    // Drive underlying ViImplementation2
+    getVii()->next();
+
+    // Synchronize own VisBuffer
+    configureNewSubchunk();
+
+    return;
+  }
+
+};
+
+class DataSwappingTVILayerFactory : public ViiLayerFactory
+{
+
+public:
+
+  DataSwappingTVILayerFactory()
+  {
+  }
+
+  virtual ~DataSwappingTVILayerFactory() {};
+
+protected:
+
+  virtual ViImplementation2 * createInstance(ViImplementation2* vii0) const
+  {
+    ViImplementation2 *vii = new DataSwappingTVI(vii0);
+    return vii;
+  }
+};
+
+class DataAccessTest : public ::testing::Test
+{
+public:
+  DataAccessTest()
+  {
+    //Use the system temp dir, if not defined resort to /tmp
+    char * sys_tmpdir = getenv("TMPDIR");
+    if(sys_tmpdir != NULL)
+      strcpy(tmpdir_p, sys_tmpdir);
+    else
+      strcpy(tmpdir_p, "/tmp");
+    stpcpy (tmpdir_p+strlen(tmpdir_p), "/test_tViiLayerFactory_XXXXXX");
+    mkdtemp(tmpdir_p);
+
+    msf_p = std::move(
+        std::unique_ptr<MsFactory>(
+            new MsFactory(String::format("%s/DataAccessTest.ms", tmpdir_p))));
+  }
+
+  void removeCorrectedData()
+  {
+    msf_p->removeColumn(MS::CORRECTED_DATA);
+  }
+
+  void visitIterator(std::function<void(void)> visitor)
+  {
+    for (vi_p->originChunks (); vi_p->moreChunks(); vi_p->nextChunk()){
+      for (vi_p->origin(); vi_p->more (); vi_p->next()){
+        visitor();
+      }
+    }
+  }
+
+  void addSwappingDataTVI()
+  {
+    withSwappingDataTVI_p = true;
+  }
+  
+  void createTVIs()
+  {
+    //Create MS using the simulator MsFactory
+    pair<MeasurementSet *, Int> p = msf_p->createMs();
+    ms_p.reset(p.first);
+
+    //Create a disk layer type VI Factory
+    IteratingParameters ipar;
+    VisIterImpl2LayerFactory diskItFac(ms_p.get(),ipar,false);
+
+    //Create a SwappingDataTVI Factory if requested
+    std::unique_ptr<DataSwappingTVILayerFactory> swapFac;
+    if(withSwappingDataTVI_p)
+      swapFac.reset(new DataSwappingTVILayerFactory());
+
+    //Create a layered factory with all the layers of factories
+    size_t nFac = 1;
+    if(withSwappingDataTVI_p) 
+      nFac++;
+    Vector<ViiLayerFactory*> facts(nFac);
+    facts[0]=&diskItFac;
+    if(withSwappingDataTVI_p)
+      facts[1]= swapFac.get();
+
+    //Finally create the top VI
+    vi_p.reset(new VisibilityIterator2(facts));
+
+    vb_p = vi_p->getVisBuffer();
+  }
+
+  ~DataAccessTest()
+  {
+    //This will recursively remove everything in the directory
+    nftw(tmpdir_p, removeFile, 64, FTW_DEPTH | FTW_PHYS);
+  }
+
+  char tmpdir_p[100];
+  bool withSwappingDataTVI_p = false;
+  std::unique_ptr<casa::vi::test::MsFactory> msf_p;
+  std::unique_ptr<VisibilityIterator2> vi_p;
+  VisBuffer2 * vb_p;
+  std::unique_ptr<MeasurementSet> ms_p;
+};
  
+TEST_F(DataAccessTest, AccessCorrectedData)
+{
+  createTVIs();
+
+  //Traverse the iterator accessing the corrected data cube
+  visitIterator([&]() -> void {vb_p->visCubeCorrected().shape();std::cout<< vb_p->getSubchunk()<<std::endl;});
+}
+
+TEST_F(DataAccessTest, AccessCorrectedDataWhenMissing)
+{
+//  removeCorrectedData();
+
+//  createTVIs();
+
+  //Traverse the iterator accessing the corrected data cube. This should
+  //throw, since it has been removed from the MS.
+//  ASSERT_THROW(visitIterator([&]() -> void {vb_p->visCubeCorrected().shape();}),
+//               AipsError);
+}
+
+TEST_F(DataAccessTest, AccessCorrectedDataInSwappingDataTVI)
+{
+  removeCorrectedData();
+
+  addSwappingDataTVI();
+  
+  createTVIs();
+
+  //Traverse the iterator accessing the corrected data cube. This works
+  //despite removing the corrected data from disk because the upper TVI layer
+  //delivers data when corrected data is requested.
+  SCOPED_TRACE("LL");
+  visitIterator([&]() -> void {SCOPED_TRACE("II"); vb_p->visCubeCorrected().shape();});
+}
