554c1554e85ad54b8130d6797a764544267823bf
Ville Suoranta <vsuorant@nrao.edu>
2018-03-22 14:27:45 -0400

    Enforce telemetry logfile size limits

--- gcwrap/python/scripts/init_telemetry.py
+++ gcwrap/python/scripts/init_telemetry.py
@@ -7,12 +7,14 @@ import __casac__
 import fnmatch
 import os
 from casac import casac
+import TelemetryLogMonitor
 
 telemetryhelper = casac.telemetryhelper()
 hostid = telemetryhelper.getUniqueId()
 
 logdir = casa['dirs']['rc']
 logpattern = 'casastats-' + hostid + '*.log'
+
 def submitStatistics():
     casalog.poststat("Stop CASA")
     print "Submitting telemetry"
@@ -26,6 +28,17 @@ casa['state']['telemetry-enabled'] = False
 
 casa_util = __casac__.utils.utils()
 rcTelemetryFlag = str.upper(casa_util.getrc("EnableTelemetry"))
+
+# Size limit for the telemetry logs
+tLogSizeLimit = 0
+try:
+    tLogSizeLimit = int(casa_util.getrc("tLogSizeLimit"))
+except:
+    pass
+if (tLogSizeLimit == None or tLogSizeLimit == 0):
+    tLogSizeLimit = 10000
+# File size check interval
+tLogSizeInterval = 60
 
 if ( casa['flags'].telemetry or
     (os.environ.has_key('CASA_ENABLE_TELEMETRY') and
@@ -46,14 +59,25 @@ if ( casa['flags'].telemetry or
 
      logfiles.sort(reverse=True)
 
+     # Size of the existing (non-active) logfiles
+     inactiveTLogSize = 0
+
      if (logfiles and logfiles[0] != None):
          print "Found an existing telemetry logfile. Appending telemetry data to " + casa['dirs']['rc'] + "/" + logfiles[0]
          casa['files']['telemetry-logfile'] = casa['dirs']['rc'] + "/" + logfiles[0]
+         for i in range(1, len(logfiles)):
+             inactiveTLogSize = inactiveTLogSize + os.path.getsize(casa['dirs']['rc'] + "/" + logfiles[i])/1024
+         print "Inactive log size: " + str(inactiveTLogSize)
      else :
          print "Creating a new telemetry file"
          casa['files']['telemetry-logfile'] = casa['dirs']['rc'] + '/casastats-' + hostid + '-' + time.strftime("%Y%m%d-%H%M%S", time.gmtime()) + '.log'
 
      # Submit statistics at shutdown
      add_shutdown_hook(submitStatistics)
+
+     # Subtract the inactive log sizes from the total log file size limit
+     tLogSizeLimit = tLogSizeLimit - inactiveTLogSize
+     tLogMonitor = TelemetryLogMonitor.TelemetryLogMonitor()
+     tLogMonitor.start(casa['files']['telemetry-logfile'],tLogSizeLimit, tLogSizeInterval, casa)
 
      print "Telemetry initialized."
