fb9b6d2a2a39b495d5f8d843d9540bea5ac233e9
Brian Kent <bkent@nrao.edu>
2018-04-20 13:30:41 -0400

    Updated pipeline regression test values

--- gcwrap/python/scripts/regressions/pipelineTest_regression.py
+++ gcwrap/python/scripts/regressions/pipelineTest_regression.py
@@ -6,15 +6,14 @@ import sys, traceback
 import numpy as np
 import pipeline
 
-
-
-pathname=os.environ.get('CASAPATH').split()[0]
+pathname = os.environ.get('CASAPATH').split()[0]
 rootdatapath = pathname+'/data/regression/pipeline/'
 
 
 '''Initial VLA pipeline regression
    B. Kent, May 2015
-   Last update September 1, 2015
+   Update September 1, 2015
+   Update April 20, 2018
 '''
 
 THISHOME  = "working/"
@@ -24,7 +23,7 @@ startProc=0.0
 endProc=0.0
 regstate = True
 standard_file = rootdatapath + 'VLApipeline44-standard'
-#MIN_CASA_REVISION = 36095
+# MIN_CASA_REVISION = 36095
 
 
 
@@ -39,7 +38,7 @@ EPS       = 1e-5  # Logical "zero"
 #
 def pipeline_regression():
     global regstate
-    #global MIN_CASA_REVISION
+    # global MIN_CASA_REVISION
         
     
     #revision = int(casadef.subversion_revision)
@@ -54,7 +53,7 @@ def pipeline_regression():
     
     
     
-    #ASDM      = "/export/home/icarus_2/awells/CASA_stable/data/regression/foo/vla_pipeline_data/rawdata/13A-537.sb24066356.eb24324502.56514.05971091435"
+    # ASDM      = "/export/home/icarus_2/awells/CASA_stable/data/regression/foo/vla_pipeline_data/rawdata/13A-537.sb24066356.eb24324502.56514.05971091435"
     ASDM = rootdatapath  + "13A-537.sb24066356.eb24324502.56514.05971091435"
     try:
         import pipeline.recipes.hifv as hifv
@@ -62,7 +61,7 @@ def pipeline_regression():
         print e
         
     
-    #Check to see if the ASDM exists
+    # Check to see if the ASDM exists
     if not os.path.exists(ASDM):
         print "Unable to open ASDM " + ASDM
 	regstate=False
@@ -70,7 +69,7 @@ def pipeline_regression():
     else:
         print "Using " + ASDM
     
-    #Run the CASA VLA Pipeline standard recipe
+    # Run the CASA VLA Pipeline standard recipe
     try:
         hifv.hifv([ASDM], importonly=False)
     except Exception, e:
@@ -106,7 +105,7 @@ def stats():
         regstate=False
     
     try:
-        #Open context
+        # Open context
         context = pipeline.Pipeline(context='last').context
         regstate=True
         print >>logfile,"VLA pipeline context created."
@@ -123,30 +122,25 @@ def stats():
         print >>logfile, e
         print e
 
-    #Test fluxscale values
-    #Test that hifv_fluxboot stage was the 13th stage run (index 12)
+    # Test fluxscale values
+    # Test that hifv_fluxboot stage was the 13th stage run (index 12)
     try:
-        #Open context of regression pipeline run
+        # Open context of regression pipeline run
         context = pipeline.Pipeline(context='last').context
         fluxlist = context.results[12].read()[0].flux_densities
-        rtol=1.0e-5  #Relative Tolerance
-        atol=1.0e-8  #Absolute Tolerance
+        rtol = 1.0e-5  #Relative Tolerance
+        atol = 1.0e-8  #Absolute Tolerance
+
+        # value_compare43 = 0.6934577681171487
+        # value_compare = 0.716367318068  # CASA 4.5
+        # value_compare = 0.717857716108  # CASA 4.6
+        # value_compare = 0.716364780148  # CASA 4.6.144 r36095, pipeline r36209 (trunk)
+        # value_compare = 0.718457023749  # CASA-test 5.0.11-DEV (r38177) 
+        # value_compare = 0.71857779577   # CASA-prerelease 5.0.0-187, pipeline r40156
+        # value_compare = 0.718551806637  # CASA-prerelease 5.1.0-34, pipeline r40615
+        # value_compare = 0.718519030402  # CASA-prerelease 5.3.0-26, pipeline r40909
+        value_compare = 0.718358266974 # CASA-prerelease 5.3.0-109, pipeline r41386
         
-        #Open context "standard" for comparison
-        #Test commit
-        #standard_context = pipeline.Pipeline(context=standard_file+'.context', path_overrides={'name':standard_file, 'output_dir':os.getcwd()}).context
-        #standard_fluxlist = standard_context.results[12].read()[0].flux_densities
-        #value_compare43 = 0.6934577681171487
-        #value_compare = 0.716367318068  #CASA 4.5
-        #value_compare = 0.717857716108  #CASA 4.6
-        #value_compare = 0.716364780148  #CASA 4.6.144 r36095, pipeline r36209 (trunk)
-        #value_compare = 0.718457023749  #CASA-test 5.0.11-DEV (r38177) 
-        #value_compare = 0.71857779577  #CASA-prerelease 5.0.0-187, pipeline r40156
-        #value_compare = 0.718551806637 #CASA-prerelease 5.1.0-34, pipeline r40615
-        value_compare = 0.718519030402 #CASA-prerelease 5.3.0-26, pipeline r40909
-        
-        
-        #result_bool = np.isclose(fluxlist[0][0], standard_fluxlist[0][0], rtol=rtol, atol=atol, equal_nan=False)
         result_bool = np.isclose(fluxlist[0][0], value_compare, rtol=rtol, atol=atol, equal_nan=False)
         
         print >>logfile, "Accepted test value is: ", value_compare, " from CASA-prerelease 5.3.0-26, pipeline r40909 (trunk)"
@@ -176,45 +170,6 @@ def stats():
         print >>logfile, e
         print e
 
-    '''
-    #Test flagging values
-    #Test flagging statistics from the final applycal stage 
-    try:
-        #Open context of regression pipeline run
-        context = pipeline.Pipeline(context='last').context
-        flagsummary= context.results[14].read()[0].flagsummary
-        
-        #Open context "standard" for comparison
-        
-        standard_context = pipeline.Pipeline(context=standard_file+'.context', path_overrides={'name':standard_file, 'output_dir':os.getcwd()}).context
-        standard_flagsummary = standard_context.results[14].read()[0].flagsummary
-        try:
-            assert cmp(flagsummary, standard_flagsummary) == 0
-            regstate=True
-            print >>logfile,"hifv_applycals flagsummary dictionaries match."
-            print "hifv_applycals flagsummary dictionaries match."
-            print >>logfile,"hifv_applycals test PASSED."
-            print "hifv_applycals test PASSED."
-        except Exception, e:
-            regstate=False
-            print >>logfile,"hifv_applycals flagsummary dictionaries do NOT match."
-            print "hifv_applycals flagsummary dictionaries do NOT match."
-            print >>logfile,"hifv_applycals test FAILED."
-            print "hifv_applycals test FAILED."
-            print >>logfile, e
-            print e
-            
-
-    except Exception, e:
-        regstate=False
-        print >>logfile,"hifv_fluxboot values are not within tolerances."
-        print "hifv_fluxboot values are not within tolerances."
-        print >>logfile,"hifv_fluxboot test FAILED."
-        print "hifv_fluxboot test FAILED."
-        print >>logfile, e
-        print e
-    '''
-
     logfile.close()
 
 def main():
@@ -225,12 +180,11 @@ def main():
         print "Interrupt requested...exiting"
     except Exception:
         traceback.print_exc(file=sys.stdout)
-    #sys.exit(0)
 
 if __name__ == "__main__":
     main()
     print "Regstate:" , regstate
     if regstate:
-    	print "Regression PASSED"
+        print "Regression PASSED"
     else:
-    	print "Regression FAILED"
+        print "Regression FAILED"
