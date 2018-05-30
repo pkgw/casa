dbae98956020067c46c4d7c636efe5c3dd940fcf
Ville Suoranta <vsuorant@nrao.edu>
2018-05-30 11:07:17 -0400

    Fix telemetry fileformat

--- gcwrap/tools/casa/logsink_cmpt.cc
+++ gcwrap/tools/casa/logsink_cmpt.cc
@@ -297,7 +297,7 @@ bool logsink::poststat(const std::string& message,
                 std::time_t convertedtime = std::chrono::system_clock::to_time_t(timestamp);
                 char formattedtime[20];
                 strftime(formattedtime, 20, "%Y-%m-%d %H:%M:%S", localtime(&convertedtime));
-                tlog << formattedtime << " ::" << telemetryLoggerPid<< ":: "<< origin << " : " << message << "\n";
+                tlog << formattedtime << " :: " << telemetryLoggerPid<< " :: "<< origin << " :: " << message << "\n";
                 tlog.close();
             } catch (const std::exception& e) { // caught by reference to base
                 std::cout << "Writing stats failed: '" << e.what() << "'\n";
