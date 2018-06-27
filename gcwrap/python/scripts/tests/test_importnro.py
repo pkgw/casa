140f2c873fd43b124e127f9b969116a478b7cda4
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2018-06-27 23:36:01 +0900

    Add test to check temperature value and unit

--- gcwrap/python/scripts/tests/test_importnro.py
+++ gcwrap/python/scripts/tests/test_importnro.py
@@ -123,8 +123,8 @@ class importnro_test(unittest.TestCase):
         # check subtables
         self._check_optional_subtables(self.outfile)
         
-        # check pressure unit and value
-        self._check_atm_pressure(self.outfile)
+        # check WEATHER table
+        self._check_weather(self.outfile)
         
     def _check_weights(self, vis):
         _tb = gentools(['tb'])[0]
@@ -253,28 +253,34 @@ class importnro_test(unittest.TestCase):
         self.assertLessEqual(elevation['value'], 90.0, msg='Elevation is above the upper limit (> 90deg). {}'.format(msg))
         self.assertGreaterEqual(elevation['value'], 0.0, msg='Elevation is below the lower limit (< 0deg). {}'.format(msg))
 
-    def _check_atm_pressure(self, vis):
+    def _check_weather(self, vis):
+        # check PRESSURE
+        self._check_weather_column(vis, 'PRESSURE', 'hPa', 400.0, 1100.0)
+        
+        # check TEMPERATURE
+        self._check_weather_column(vis, 'TEMPERATURE', 'K', 243.0, 313.0)
+        
+    def _check_weather_column(self, vis, colname, unit, value_min, value_max):
         weather_table = os.path.join(vis, 'WEATHER')
         tb.open(weather_table)
         try:
-            # PRESSURE column should exist
-            self.assertTrue('PRESSURE' in tb.colnames())
+            # column should exist
+            self.assertTrue(colname in tb.colnames())
             
-            # unit should be hPa
-            colkeys = tb.getcolkeywords('PRESSURE')
+            # unit check
+            colkeys = tb.getcolkeywords(colname)
             self.assertTrue('QuantumUnits' in colkeys)
-            pressure_unit = colkeys['QuantumUnits'][0]
-            print('Pressure unit is {0}'.format(pressure_unit))
-            self.assertEqual(pressure_unit, 'hPa')
+            column_unit = colkeys['QuantumUnits'][0]
+            print('{0} unit is {1}'.format(colname, column_unit))
+            self.assertEqual(column_unit, unit)
             
             # value should be in reasonable range
-            pressure_min = 400.0
-            pressure_max = 1100.0
-            pressure_value = tb.getcol('PRESSURE')
-            self.assertTrue(numpy.all(pressure_min < pressure_value))
-            self.assertTrue(numpy.all(pressure_value < pressure_max))
+            column_value = tb.getcol(colname)
+            self.assertTrue(numpy.all(value_min < column_value))
+            self.assertTrue(numpy.all(column_value < value_max))
         finally:
             tb.close()
+        
 
 def suite():
     return [importnro_test]
