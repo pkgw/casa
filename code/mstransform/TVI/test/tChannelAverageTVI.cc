ae405469a8e5fa5b3bb2439475f4038d279fed99
Cesar Enrique Garcia Dabo <cgarcia@eso.org>
2018-07-05 12:13:06 +0200

    Adds unit tests to check that output SPWs are as expected
    After removing the spw parsing these tests verify that the
    TVI is getting the proper info regarding SPWs and channels
    from the underlying TVI and that the output SPWs are correct.
    Fixes partially CAS-9679

--- code/mstransform/TVI/test/tChannelAverageTVI.cc
+++ code/mstransform/TVI/test/tChannelAverageTVI.cc
@@ -24,6 +24,8 @@
 #include <limits>
 #include <mstransform/TVI/test/tChannelAverageTVI.h>
 #include <msvis/MSVis/SimpleSimVi2.h>
+#include <msvis/MSVis/PassThroughTVI.h>
+#include <msvis/MSVis/test/MsFactory.h>
 
 using namespace std;
 using namespace casa;
@@ -426,50 +428,271 @@ TEST(ChannelAverageTVIExecuteSimulatedTest, UniformMS)
     }
 }
 
+ChannelAverageTVISpwChannTest::ChannelAverageTVISpwChannTest() :
+    MsFactoryTVITester("ChannelAverageTVIMSSelTest","MSFactoryCreated"),
+    useMSSelection_p(false), addPassThroughTVI_p(false), addExtraAvgTVI_p(false)
+{
+}
+
+void ChannelAverageTVISpwChannTest::useMSSelection(bool use)
+{
+    useMSSelection_p = use;
+}
+
+void ChannelAverageTVISpwChannTest::addPassThroughTVI(bool use)
+{
+    addPassThroughTVI_p = use;
+}
+
+void ChannelAverageTVISpwChannTest::addExtraAvgTVI(bool use)
+{
+    addExtraAvgTVI_p = use;
+}
+
+void ChannelAverageTVISpwChannTest::createTVIs()
+{
+    //Setting the parameters to generate a synthetic MS 
+    double timeInterval = 10;
+    double timeSpan = 100;
+    msf_p->setTimeInfo (0, timeSpan, timeInterval);
+    msf_p->addAntennas(6);
+    //Adding two spectral windows with different number of channels
+    int nChannels = 100;
+    double frequency = 1e11;
+    double frequencyDelta = 1e9;
+    std::string stokes("XX YY");
+    msf_p->addSpectralWindow("SPW0", nChannels, 
+                             frequency, frequencyDelta, stokes);
+    nChannels = 50;
+    frequency = 2e11;
+    msf_p->addSpectralWindow("SPW1", nChannels, 
+                             frequency, frequencyDelta, stokes);
+    msf_p->addFeeds (10); //Need antenna and spw to be set up first
+
+    //Creates the synthetic MS 
+    createMS();
+
+    //If there is selection, create a new MS as a selection on the original one
+    std::unique_ptr<casacore::MeasurementSet> msSelected;
+    std::shared_ptr<vi::FrequencySelectionUsingChannels> freqSel;
+    if(useMSSelection_p)
+    {
+        MSSelection thisSelection;
+        std::string spwSelection("0:1~40");
+        thisSelection.setSpwExpr(spwSelection);
+        TableExprNode exprNode = thisSelection.toTableExprNode(ms_p.get());
+        msSelected.reset(new MeasurementSet((*ms_p.get())(exprNode)));
+        freqSel = std::make_shared<vi::FrequencySelectionUsingChannels>();
+        freqSel->add(thisSelection, ms_p.get());
+    }
+
+    //Create a VI Factory that access directly the MS (or the selected MS)
+    IteratingParameters ipar;
+    std::unique_ptr<VisIterImpl2LayerFactory> diskItFac;
+    if(useMSSelection_p)
+    {
+        diskItFac.reset(new VisIterImpl2LayerFactory(msSelected.get(),ipar, false));
+        diskItFac->setFrequencySelection(freqSel);
+    }
+    else
+        diskItFac.reset(new VisIterImpl2LayerFactory(ms_p.get(),ipar, false));
+
+    //Create a ChannelAverageTVI Factory
+    std::unique_ptr<ChannelAverageTVILayerFactory> chanAvgFac;
+    casacore::Record configuration;
+    configuration.define ("chanbin", 5);
+    //configuration.define ("spw", "0:1;3;5;7;9");
+    chanAvgFac.reset(new ChannelAverageTVILayerFactory(configuration));
+
+    //Create a layered factory with all the layers of factories
+    //If requested a PassThroughTVI is added to test that ChannelAverage
+    //forwards properly all the information to layers above
+    size_t nFac = 2;
+    if(addPassThroughTVI_p)
+        nFac++;
+    if(addExtraAvgTVI_p)
+        nFac++;
+    std::vector<ViiLayerFactory*> factories(nFac);
+    factories[0]= diskItFac.get();
+    factories[1]= chanAvgFac.get();
+    std::unique_ptr<PassThroughTVILayerFactory> passThroughFactory;
+    if(addPassThroughTVI_p)
+    {
+        passThroughFactory.reset(new PassThroughTVILayerFactory());
+        factories[2]= passThroughFactory.get();
+    }
+    //Add a second averaging layer if requested
+    std::unique_ptr<ChannelAverageTVILayerFactory> chanAvgFac2;
+    if(addExtraAvgTVI_p)
+    {
+        casacore::Record configuration2;
+        Array<int> chanArr(IPosition(1,2));
+        chanArr[0] = 2;
+        chanArr[1] = 5;
+        configuration2.define ("chanbin", chanArr);
+        chanAvgFac2.reset(new ChannelAverageTVILayerFactory(configuration2));
+        factories[3]= chanAvgFac2.get();
+    }
+    
+    //Finally create the VI resulting from all the layered TVIs
+    instantiateVI(factories);
+}
+
+TEST_F(ChannelAverageTVISpwChannTest, CheckOutputSpwChannels)
+{
+    createTVIs();
+    size_t nRows = 0;
+    size_t nRowsSpw0 = 0;
+    size_t nRowsSpw1 = 0;
+    //Check that after averaging with chanbin=5 the first spectral window has 20
+    //channels and the second spectral window has 10 channel
+    visitIterator([&]() -> void {auto shape = vb_p->visCube().shape(); 
+                                 if(allEQ(vb_p->spectralWindows(), 0)) //SPW0 
+                                 {
+                                     nRowsSpw0+=shape[2];
+                                     ASSERT_EQ(vb_p->nChannels(), 20);
+                                 }
+                                 else if(allEQ(vb_p->spectralWindows(), 1)) //SPW1
+                                 {
+                                     nRowsSpw1+=shape[2];
+                                     ASSERT_EQ(vb_p->nChannels(), 10);
+                                 }
+                                 ASSERT_EQ(vi_p->nSpectralWindows(), 2);
+                                 nRows+=vb_p->visCube().shape()[2];});
+
+    //All the original rows
+    size_t expectedRows = 300;
+    //The synthetic MS has half of the rows in each SPW
+    size_t expectedRowsSpw0 = expectedRows / 2; 
+    size_t expectedRowsSpw1 = expectedRows / 2;
+
+    //Check that we get the number of expected rows for each spw
+    ASSERT_EQ(nRows, expectedRows);
+    ASSERT_EQ(nRowsSpw0, expectedRowsSpw0);
+    ASSERT_EQ(nRowsSpw1, expectedRowsSpw1);
+}
+
+TEST_F(ChannelAverageTVISpwChannTest, CheckMSSelOutputSpwChannels)
+{
+    useMSSelection(true);
+    addPassThroughTVI(true);
+    createTVIs();
+    size_t nRows = 0;
+    size_t nRowsSpw0 = 0;
+    size_t nRowsSpw1 = 0;
+    //Check that after averaging with chanbin=5 and spw selection "0:1~40",
+    //the first spectral window has 8 channels 
+    //and there are no rows with the second spectral window 
+    visitIterator([&]() -> void {auto shape = vb_p->visCube().shape(); 
+    
+    std::cout<<vb_p->spectralWindows()<<vb_p->nChannels()<<std::endl;
+    std::cout<<vb_p->antenna1()<<std::endl;
+    std::cout<<vb_p->dataDescriptionIds()<<std::endl;
+    std::cout<<shape<<std::endl;
+    
+                                 if(allEQ(vb_p->spectralWindows(), 0)) //SPW0 
+                                 {
+                                     nRowsSpw0+=shape[2];
+                                     ASSERT_EQ(vb_p->nChannels(), 8);
+                                 }
+                                 //Note that as per CAS-10294 this should be 3
+                                 ASSERT_EQ(vi_p->nSpectralWindows(), 2);
+                                 nRows+=vb_p->visCube().shape()[2];});
+
+    //After selection we have only half of the original rows
+    //and all of them belong to SPW0.
+    size_t expectedRows = 150;
+    size_t expectedRowsSpw0 = expectedRows;
+    size_t expectedRowsSpw1 = 0;
+
+    //Check that we get the number of expected rows for each spw
+    ASSERT_EQ(nRows, expectedRows);
+    ASSERT_EQ(nRowsSpw0, expectedRowsSpw0);
+    ASSERT_EQ(nRowsSpw1, expectedRowsSpw1);
+}
+
+TEST_F(ChannelAverageTVISpwChannTest, CheckMSSelOutputTwoAvgSpwChannels)
+{
+    //useMSSelection(true);
+    addPassThroughTVI(true);
+    addExtraAvgTVI(true);
+    createTVIs();
+    size_t nRows = 0;
+    size_t nRowsSpw0 = 0;
+    size_t nRowsSpw1 = 0;
+    //Check that after averaging with chanbin=5 and further averaging with
+    //chanbin=(2,5), the first spectral window has 10
+    //channels and the second spectral window has 2 channels
+    visitIterator([&]() -> void {auto shape = vb_p->visCube().shape(); 
+                                 if(allEQ(vb_p->spectralWindows(), 0)) //SPW0 
+                                 {
+                                     nRowsSpw0+=shape[2];
+                                     ASSERT_EQ(vb_p->nChannels(), 10);
+                                 }
+                                 else if(allEQ(vb_p->spectralWindows(), 1)) //SPW1
+                                 {
+                                     nRowsSpw1+=shape[2];
+                                     ASSERT_EQ(vb_p->nChannels(), 2);
+                                 }
+                                 //Note that as per CAS-10294 this should be 3
+                                 ASSERT_EQ(vi_p->nSpectralWindows(), 2);
+                                 nRows+=vb_p->visCube().shape()[2];});
+
+    //All the original rows
+    size_t expectedRows = 300;
+    //The synthetic MS has half of the rows in each SPW
+    size_t expectedRowsSpw0 = expectedRows / 2;
+    size_t expectedRowsSpw1 = expectedRows / 2;
+
+    //Check that we get the number of expected rows for each spw
+    ASSERT_EQ(nRows, expectedRows);
+    ASSERT_EQ(nRowsSpw0, expectedRowsSpw0);
+    ASSERT_EQ(nRowsSpw1, expectedRowsSpw1);
+}
 
 //////////////////////////////////////////////////////////////////////////
 // main
 //////////////////////////////////////////////////////////////////////////
 int main(int argc, char **argv)
 {
-	int ret;
-	string parameter,value;
-	Record configuration;
-	Bool autoMode = true;
+    int ret;
+    string parameter,value;
+    Record configuration;
+    Bool autoMode = true;
 
-	for (unsigned short i=0;i<argc-1;i++)
-	{
-		parameter = string(argv[i]);
-		value = string(argv[i+1]);
+    for (unsigned short i=0;i<argc-1;i++)
+    {
+        parameter = string(argv[i]);
+        value = string(argv[i+1]);
 
-		if (parameter == string("-vis"))
-		{
-			configuration.define ("inputms", value);
-			autoMode = false;
-		}
-		else if (parameter == string("-spw"))
-		{
-			configuration.define ("spw", value);
-		}
-		else if (parameter == string("-chanbin"))
-		{
-			Int tmp = Int(atoi(value.c_str()));
-			configuration.define ("chanbin", tmp);
-		}
-	}
+        if (parameter == string("-vis"))
+        {
+            configuration.define ("inputms", value);
+            autoMode = false;
+        }
+        else if (parameter == string("-spw"))
+        {
+            configuration.define ("spw", value);
+        }
+        else if (parameter == string("-chanbin"))
+        {
+            Int tmp = Int(atoi(value.c_str()));
+            configuration.define ("chanbin", tmp);
+        }
+    }
 
-	if (autoMode)
-	{
-		::testing::InitGoogleTest(&argc, argv);
-		ret = RUN_ALL_TESTS();
-	}
-	else
-	{
-		ChannelAverageTVICompareTest test(configuration);
-		test.TestBody();
-		if (test.getTestResult()) ret = 0;
-		else ret = 1;
-	}
+    if (autoMode)
+    {
+        ::testing::InitGoogleTest(&argc, argv);
+        ret = RUN_ALL_TESTS();
+    }
+    else
+    {
+        ChannelAverageTVICompareTest test(configuration);
+        test.TestBody();
+        if (test.getTestResult()) ret = 0;
+        else ret = 1;
+    }
 
-	return ret;
+  return ret;
 }
