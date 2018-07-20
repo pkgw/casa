1a63bf0048c00f618eebf0bfc1cc0f417880c5c2
Ville Suoranta <vsuorant@nrao.edu>
2018-07-20 13:36:46 -0400

    Add startup message on how to disable telemetry

--- gcwrap/python/scripts/telemetry.py
+++ gcwrap/python/scripts/telemetry.py
@@ -67,7 +67,11 @@ class telemetry:
         else :
             tLogMonitor = TelemetryLogMonitor.TelemetryLogMonitor()
             tLogMonitor.start(casa['files']['telemetry-logfile'],tLogSizeLimit, tLogSizeInterval, casa)
-            print "Telemetry initialized."
+            print "Telemetry initialized. Telemetry will send anonymized usage statistics to NRAO."
+            print 'You can disable telemetry by setting environment variable with'
+            print 'export "CASA_ENABLE_TELEMETRY=false'
+            print 'or by adding the following line in your ~/.casarc file:'
+            print 'EnableTelemetry: False'
 
     def setNewTelemetryFile(self):
         self.casa['files']['telemetry-logfile'] =  self.logdir + '/casastats-' + self.casaver +'-'  + self.hostid + "-" + time.strftime("%Y%m%d-%H%M%S", time.gmtime()) + '.log'
