7cb1b2ead2481fe4d95c3b0da7050ccb419e6482
George Moellenbrock <gmoellen@nrao.edu>
2018-12-05 22:24:51 -0700

    Introduce Xparang and Xfparang classes, currently same implementations as GlinXph; will differentiate them in basis-agnostic way next.

--- code/synthesis/MeasurementComponents/XJones.cc
+++ code/synthesis/MeasurementComponents/XJones.cc
@@ -994,6 +994,692 @@ void XfJones::initSolvePar() {
 
 
 
+// **********************************************************
+//  XparangJones Implementations
+//
+
+XparangJones::XparangJones(VisSet& vs) :
+  VisCal(vs),             // virtual base
+  VisMueller(vs),         // virtual base
+  XJones(vs),             // immediate parent
+  QU_()
+{
+  if (prtlev()>2) cout << "Xparang::Xparang(vs)" << endl;
+}
+
+XparangJones::XparangJones(String msname,Int MSnAnt,Int MSnSpw) :
+  VisCal(msname,MSnAnt,MSnSpw),             // virtual base
+  VisMueller(msname,MSnAnt,MSnSpw),         // virtual base
+  XJones(msname,MSnAnt,MSnSpw),             // immediate parent
+  QU_()
+{
+  if (prtlev()>2) cout << "Xparang::Xparang(msname,MSnAnt,MSnSpw)" << endl;
+}
+
+XparangJones::XparangJones(const MSMetaInfoForCal& msmc) :
+  VisCal(msmc),             // virtual base
+  VisMueller(msmc),         // virtual base
+  XJones(msmc),             // immediate parent
+  QU_()
+{
+  if (prtlev()>2) cout << "Xparang::Xparang(msmc)" << endl;
+}
+
+XparangJones::XparangJones(const Int& nAnt) :
+  VisCal(nAnt), 
+  VisMueller(nAnt),
+  XJones(nAnt),
+  QU_()
+{
+  if (prtlev()>2) cout << "Xparang::Xparang(nAnt)" << endl;
+}
+
+XparangJones::~XparangJones() {
+  if (prtlev()>2) cout << "Xparang::~Xparang()" << endl;
+}
+
+void XparangJones::setApply(const Record& apply) {
+
+  XJones::setApply(apply);
+
+  // Force calwt to false 
+  calWt()=false;
+
+}
+
+
+
+void XparangJones::selfGatherAndSolve(VisSet& vs, VisEquation& ve) {
+
+  if (prtlev()>4) cout << "   GlnXph::selfGatherAndSolve(ve)" << endl;
+
+  // Inform logger/history
+  logSink() << "Solving for " << typeName()
+            << LogIO::POST;
+
+  // Initialize the svc according to current VisSet
+  //  (this counts intervals, sizes CalSet)
+  Vector<Int> nChunkPerSol;
+  Int nSol = sizeUpSolve(vs,nChunkPerSol);
+
+  // Create the Caltable
+  createMemCalTable();
+
+  // The iterator, VisBuffer
+  VisIter& vi(vs.iter());
+  VisBuffer vb(vi);
+
+  //  cout << "nSol = " << nSol << endl;
+  //  cout << "nChunkPerSol = " << nChunkPerSol << endl;
+
+  Int nGood(0);
+  vi.originChunks();
+  for (Int isol=0;isol<nSol && vi.moreChunks();++isol) {
+
+    // Arrange to accumulate
+    VisBuffAccumulator vba(nAnt(),preavg(),false);
+    
+    for (Int ichunk=0;ichunk<nChunkPerSol(isol);++ichunk) {
+
+      // Current _chunk_'s spw
+      Int spw(vi.spectralWindow());
+
+      // Abort if we encounter a spw for which a priori cal not available
+      if (!ve.spwOK(spw))
+        throw(AipsError("Pre-applied calibration not available for at least 1 spw. Check spw selection carefully."));
+
+
+      // Collapse each timestamp in this chunk according to VisEq
+      //  with calibration and averaging
+      for (vi.origin(); vi.more(); vi++) {
+
+        // Force read of the field Id
+        vb.fieldId();
+
+        // This forces the data/model/wt I/O, and applies
+        //   any prior calibrations
+        ve.collapse(vb);
+
+        // If permitted/required by solvable component, normalize
+        if (normalizable())
+	  vb.normalize();
+
+	// If this solve not freqdep, and channels not averaged yet, do so
+	if (!freqDepMat() && vb.nChannel()>1)
+	  vb.freqAveCubes();
+
+        // Accumulate collapsed vb in a time average
+        vba.accumulate(vb);
+      }
+      // Advance the VisIter, if possible
+      if (vi.moreChunks()) vi.nextChunk();
+
+    }
+
+    // Finalize the averged VisBuffer
+    vba.finalizeAverage();
+
+    // The VisBuffer to solve with
+    VisBuffer& svb(vba.aveVisBuff());
+
+    // Establish meta-data for this interval
+    //  (some of this may be used _during_ solve)
+    //  (this sets currSpw() in the SVC)
+    Bool vbOk=syncSolveMeta(svb,-1);
+
+    if (vbOk && svb.nRow()>0) {
+
+      // solve for the X-Y phase term in the current VB
+      solveOneVB(svb);
+
+      nGood++;
+    }
+
+    keepNCT();
+    
+  }
+  
+  logSink() << "  Found good "
+            << typeName() << " solutions in "
+            << nGood << " intervals."
+            << LogIO::POST;
+
+  // Store whole of result in a caltable
+  if (nGood==0)
+    logSink() << "No output calibration table written."
+              << LogIO::POST;
+  else {
+
+    // Do global post-solve tinkering (e.g., phase-only, normalization, etc.)
+    globalPostSolveTinker();
+
+    // write the table
+    storeNCT();
+  }
+
+}
+
+// Handle trivial vbga
+void XparangJones::selfSolveOne(VisBuffGroupAcc& vbga) {
+
+  // Expecting only on VB in the vbga (with many times)
+  if (vbga.nBuf()!=1)
+    throw(AipsError("XparangJones can't process multi-vb vbga."));
+
+  // Call single-VB specialized solver with the one vb
+  this->solveOneVB(vbga(0));
+
+}
+
+// SDBList (VI2) version
+void XparangJones::selfSolveOne(SDBList& sdbs) {
+
+  // Expecting multiple SDBs (esp. in time)
+  if (sdbs.nSDB()==1)
+    throw(AipsError("XparangJones needs multiple SDBs"));
+
+  // Call single-VB specialized solver with the one vb
+  this->solveOne(sdbs);
+
+}
+
+// Solve for the X-Y phase from the cross-hand's slope in R/I
+void XparangJones::solveOneVB(const VisBuffer& vb) {
+
+  // ensure
+  if (QU_.shape()!=IPosition(2,2,nSpw())) {
+    QU_.resize(2,nSpw());
+    QU_.set(0.0);
+  }
+
+  Int thisSpw=spwMap()(vb.spectralWindow());
+  
+  // We are actually solving for all channels simultaneously
+  solveCPar().reference(solveAllCPar());
+  solveParOK().reference(solveAllParOK());
+  solveParErr().reference(solveAllParErr());
+  solveParSNR().reference(solveAllParSNR());
+  
+  // Fill solveCPar() with 1, nominally, and flagged
+  solveCPar()=Complex(1.0);
+  solveParOK()=false;
+
+  Int nChan=vb.nChannel();
+  //  if (nChan>1)
+  //    throw(AipsError("X-Y phase solution NYI for channelized data"));
+
+  // Find number of timestamps in the VB
+  Vector<uInt> ord;
+  Int nTime=genSort(ord,vb.time(),Sort::Ascending,Sort::NoDuplicates);
+
+  Matrix<Double> x(nTime,nChan,0.0),y(nTime,nChan,0.0),wt(nTime,nChan,0.0),sig(nTime,nChan,0.0);
+  Matrix<Bool> mask(nTime,nChan,false);
+
+  mask.set(false);
+  Complex v(0.0);
+  Float wt0(0.0);
+  Int iTime(-1);
+  Double currtime(-1.0);
+  for (Int irow=0;irow<vb.nRow();++irow) {
+    if (!vb.flagRow()(irow) &&
+	vb.antenna1()(irow)!=vb.antenna2()(irow)) {
+
+      // Advance time index when we see a new time
+      if (vb.time()(irow)!=currtime) {
+	++iTime;
+	currtime=vb.time()(irow); // remember the new current time
+      }
+
+      // Weights not yet chan-dep
+      wt0=(vb.weightMat()(1,irow)+vb.weightMat()(2,irow));
+      if (wt0>0.0) {
+
+	for (Int ich=0;ich<nChan;++ich) {
+	  if (!vb.flag()(ich,irow)) {
+	    v=vb.visCube()(1,ich,irow)+conj(vb.visCube()(2,ich,irow));
+	    x(iTime,ich)+=Double(wt0*real(v));
+	    y(iTime,ich)+=Double(wt0*imag(v));
+	    wt(iTime,ich)+=Double(wt0);
+	  }
+	}
+      }
+    }
+  }
+
+  // Normalize data by accumulated weights
+  for (Int itime=0;itime<nTime;++itime) {
+    for (Int ich=0;ich<nChan;++ich) {
+      if (wt(itime,ich)>0.0) {
+	x(itime,ich)/=wt(itime,ich);
+	y(itime,ich)/=wt(itime,ich);
+	sig(itime,ich)=sqrt(1.0/wt(itime,ich));
+	mask(itime,ich)=true;
+      }
+      else
+	sig(itime,ich)=DBL_MAX;    // ~zero weight
+    }
+  }
+
+  // Solve for each channel
+  Vector<Complex> Cph(nChan);
+  Cph.set(Complex(1.0,0.0));
+  Float currAmb(1.0);
+  Bool report(false);
+  for (Int ich=0;ich<nChan;++ich) {
+
+    if (sum(wt.column(ich))>0.0) {
+      report=true;
+      LinearFit<Double> phfitter;
+      Polynomial<AutoDiff<Double> > line(1);
+      phfitter.setFunction(line);
+      Vector<Bool> m(mask.column(ich));
+
+      // Fit shallow and steep, and always prefer shallow
+      
+      // Assumed shallow solve:
+      Vector<Double> solnA;
+      solnA.assign(phfitter.fit(x.column(ich),y.column(ich),sig.column(ich),&m));
+
+      // Assumed steep solve:
+      Vector<Double> solnB;
+      solnB.assign(phfitter.fit(y.column(ich),x.column(ich),sig.column(ich),&m));
+
+      Double Xph(0.0);
+      if (abs(solnA(1))<abs(solnB(1))) {
+	Xph=atan(solnA(1));
+      }
+      else {
+	Xph=atan(1.0/solnB(1));
+      }
+
+      Cph(ich)=currAmb*Complex(DComplex(cos(Xph),sin(Xph)));
+
+      // Watch for and remove ambiguity changes which can
+      //  occur near Xph~= +/-90 deg (the atan above can jump)
+      //  (NB: this does _not_ resolve the amb; it merely makes
+      //   it consistent within the spw)
+      if (ich>0) {
+	// If Xph changes by more than pi/2, probably a ambig jump...
+	Float dang=abs(arg(Cph(ich)/Cph(ich-1)));
+	if (dang > (C::pi/2.)) {
+	  Cph(ich)*=-1.0;   // fix this one
+	  currAmb*=-1.0;    // reverse currAmb, so curr amb is carried forward
+	  //	  cout << "  Found XY phase ambiguity jump at chan=" << ich << " in spw=" << currSpw();
+	}
+      }
+
+      //      cout << " (" << currAmb << ")";
+      //      cout << endl;
+
+
+      // Set all antennas with this X-Y phase (as a complex number)
+      solveCPar()(Slice(0,1,1),Slice(ich,1,1),Slice())=Cph(ich);
+      solveParOK()(Slice(),Slice(ich,1,1),Slice())=true;
+    }
+    else {
+      solveCPar()(Slice(0,1,1),Slice(ich,1,1),Slice())=Complex(1.0);
+      solveParOK()(Slice(),Slice(ich,1,1),Slice())=false;
+    }
+  }
+
+  if (report)
+    cout << endl 
+	 << "Spw = " << thisSpw
+	 << " (ich=" << nChan/2 << "/" << nChan << "): " << endl
+	 << " X-Y phase = " << arg(Cph[nChan/2])*180.0/C::pi << " deg." << endl;
+      
+
+  // Now fit for the source polarization
+  {
+
+    Vector<Double> wtf(nTime,0.0),sigf(nTime,0.0),xf(nTime,0.0),yf(nTime,0.0);
+    Vector<Bool> maskf(nTime,false);
+    Float wt0;
+    Complex v;
+    Double currtime(-1.0);
+    Int iTime(-1);
+    for (Int irow=0;irow<vb.nRow();++irow) {
+      if (!vb.flagRow()(irow) &&
+	  vb.antenna1()(irow)!=vb.antenna2()(irow)) {
+	
+	if (vb.time()(irow)!=currtime) {
+	  // Advance time index when we see a new time
+	  ++iTime;
+	  currtime=vb.time()(irow); // remember the new current time
+	}
+
+	// Weights not yet chan-dep
+	wt0=(vb.weightMat()(1,irow)+vb.weightMat()(2,irow));
+	if (wt0>0.0) {
+	  for (Int ich=0;ich<nChan;++ich) {
+	    
+	    if (!vb.flag()(ich,irow)) {
+	      // Correct x-hands for xy-phase and add together
+	      v=vb.visCube()(1,ich,irow)/Cph(ich)+vb.visCube()(2,ich,irow)/conj(Cph(ich));
+	      xf(iTime)+=Double(wt0*2.0*(vb.feed_pa(vb.time()(irow))(0)));
+	      yf(iTime)+=Double(wt0*real(v)/2.0);
+	      wtf(iTime)+=Double(wt0);
+	    }
+	  }
+	}
+      }
+    }
+    
+    // Normalize data by accumulated weights
+    for (Int itime=0;itime<nTime;++itime) {
+      if (wtf(itime)>0.0) {
+	xf(itime)/=wtf(itime);
+	yf(itime)/=wtf(itime);
+	sigf(itime)=sqrt(1.0/wtf(itime));
+	maskf(itime)=true;
+      }
+      else
+	sigf(itime)=DBL_MAX;    // ~zero weight
+    }
+    
+    // p0=Q, p1=U, p2 = real part of net instr pol offset
+    //  x is TWICE the parallactic angle
+    CompiledFunction<AutoDiff<Double> > fn;
+    fn.setFunction("-p0*sin(x) + p1*cos(x) + p2");
+
+    LinearFit<Double> fitter;
+    fitter.setFunction(fn);
+    
+    Vector<Double> soln=fitter.fit(xf,yf,sigf,&maskf);
+    
+    QU_(0,thisSpw) = soln(0);
+    QU_(1,thisSpw) = soln(1);
+
+    cout << " Fractional Poln: "
+	 << "Q = " << QU_(0,thisSpw) << ", "
+	 << "U = " << QU_(1,thisSpw) << "; "
+	 << "P = " << sqrt(soln(0)*soln(0)+soln(1)*soln(1)) << ", "
+	 << "X = " << atan2(soln(1),soln(0))*90.0/C::pi << "deg."
+	 << endl;
+    cout << " Net (over baselines) instrumental polarization: " 
+	 << soln(2) << endl;
+
+  }	
+
+}
+
+// Solve for the X-Y phase from the cross-hand's slope in R/I
+void XparangJones::solveOne(SDBList& sdbs) {
+
+  // ensure
+  if (QU_.shape()!=IPosition(2,2,nSpw())) {
+    QU_.resize(2,nSpw());
+    QU_.set(0.0);
+  }
+
+  Int thisSpw=sdbs.aggregateSpw();
+  
+  // We are actually solving for all channels simultaneously
+  solveCPar().reference(solveAllCPar());
+  solveParOK().reference(solveAllParOK());
+  solveParErr().reference(solveAllParErr());
+  solveParSNR().reference(solveAllParSNR());
+  
+  // Fill solveCPar() with 1, nominally, and flagged
+  solveCPar()=Complex(1.0);
+  solveParOK()=false;
+
+  Int nChan=sdbs.nChannels();
+
+  // Number of datapoints in fit is the number of SDBs
+  Int nSDB=sdbs.nSDB();
+
+  Matrix<Double> x(nSDB,nChan,0.0),y(nSDB,nChan,0.0),wt(nSDB,nChan,0.0),sig(nSDB,nChan,0.0);
+  Matrix<Bool> mask(nSDB,nChan,false);
+
+  mask.set(false);
+  Complex v(0.0);
+  Float wt0(0.0);
+  
+  for (Int isdb=0;isdb<nSDB;++isdb) {
+    SolveDataBuffer& sdb(sdbs(isdb));
+
+    for (Int irow=0;irow<sdb.nRows();++irow) {
+      if (!sdb.flagRow()(irow) &&
+	  sdb.antenna1()(irow)!=sdb.antenna2()(irow)) {
+	
+	for (Int ich=0;ich<nChan;++ich) {
+	  if (!sdb.flagCube()(1,ich,irow)) {
+	    wt0=sdb.weightSpectrum()(1,ich,irow);
+	    v=sdb.visCubeCorrected()(1,ich,irow);
+	    x(isdb,ich)+=Double(wt0*real(v));
+	    y(isdb,ich)+=Double(wt0*imag(v));
+	    wt(isdb,ich)+=Double(wt0);
+	  }
+	  if (!sdb.flagCube()(2,ich,irow)) {
+	    wt0=sdb.weightSpectrum()(2,ich,irow);
+	    v=conj(sdb.visCubeCorrected()(2,ich,irow));
+	    x(isdb,ich)+=Double(wt0*real(v));
+	    y(isdb,ich)+=Double(wt0*imag(v));
+	    wt(isdb,ich)+=Double(wt0);
+	  }
+	}
+      }
+    }
+  }
+
+  // Normalize data by accumulated weights
+  for (Int isdb=0;isdb<nSDB;++isdb) {
+    for (Int ich=0;ich<nChan;++ich) {
+      if (wt(isdb,ich)>0.0) {
+	x(isdb,ich)/=wt(isdb,ich);
+	y(isdb,ich)/=wt(isdb,ich);
+	sig(isdb,ich)=sqrt(1.0/wt(isdb,ich));
+	mask(isdb,ich)=true;
+      }
+      else
+	sig(isdb,ich)=DBL_MAX;    // ~zero weight
+    }
+  }
+
+  // Solve for each channel
+  Vector<Complex> Cph(nChan);
+  Cph.set(Complex(1.0,0.0));
+  Float currAmb(1.0);
+  Bool report(false);
+  for (Int ich=0;ich<nChan;++ich) {
+
+    if (sum(wt.column(ich))>0.0) {
+      report=true;
+      LinearFit<Double> phfitter;
+      Polynomial<AutoDiff<Double> > line(1);
+      phfitter.setFunction(line);
+      Vector<Bool> m(mask.column(ich));
+
+      // Fit shallow and steep, and always prefer shallow
+      
+      // Assumed shallow solve:
+      Vector<Double> solnA;
+      solnA.assign(phfitter.fit(x.column(ich),y.column(ich),sig.column(ich),&m));
+
+      // Assumed steep solve:
+      Vector<Double> solnB;
+      solnB.assign(phfitter.fit(y.column(ich),x.column(ich),sig.column(ich),&m));
+
+      Double Xph(0.0);
+      if (abs(solnA(1))<abs(solnB(1))) {
+	Xph=atan(solnA(1));
+      }
+      else {
+	Xph=atan(1.0/solnB(1));
+      }
+
+      Cph(ich)=currAmb*Complex(DComplex(cos(Xph),sin(Xph)));
+
+      // Watch for and remove ambiguity changes which can
+      //  occur near Xph~= +/-90 deg (the atan above can jump)
+      //  (NB: this does _not_ resolve the amb; it merely makes
+      //   it consistent within the spw)
+      if (ich>0) {
+	// If Xph changes by more than pi/2, probably a ambig jump...
+	Float dang=abs(arg(Cph(ich)/Cph(ich-1)));
+	if (dang > (C::pi/2.)) {
+	  Cph(ich)*=-1.0;   // fix this one
+	  currAmb*=-1.0;    // reverse currAmb, so curr amb is carried forward
+	  //	  cout << "  Found XY phase ambiguity jump at chan=" << ich << " in spw=" << currSpw();
+	}
+      }
+
+      //      cout << " (" << currAmb << ")";
+      //      cout << endl;
+
+
+      // Set all antennas with this X-Y phase (as a complex number)
+      solveCPar()(Slice(0,1,1),Slice(ich,1,1),Slice())=Cph(ich);
+      solveParOK()(Slice(),Slice(ich,1,1),Slice())=true;
+    }
+    else {
+      solveCPar()(Slice(0,1,1),Slice(ich,1,1),Slice())=Complex(1.0);
+      solveParOK()(Slice(),Slice(ich,1,1),Slice())=false;
+    }
+  }
+
+  if (report)
+    cout << endl 
+	 << "Spw = " << thisSpw
+	 << " (ich=" << nChan/2 << "/" << nChan << "): " << endl
+	 << " X-Y phase = " << arg(Cph[nChan/2])*180.0/C::pi << " deg." << endl;
+      
+
+  // Now fit for the source polarization
+  {
+
+    Vector<Double> wtf(nSDB,0.0),sigf(nSDB,0.0),xf(nSDB,0.0),yf(nSDB,0.0);
+    Vector<Bool> maskf(nSDB,false);
+    Float wt0;
+    Complex v;
+    for (Int isdb=0;isdb<nSDB;++isdb) {
+      SolveDataBuffer& sdb(sdbs(isdb));
+
+      for (Int irow=0;irow<sdb.nRows();++irow) {
+	if (!sdb.flagRow()(irow) &&
+	    sdb.antenna1()(irow)!=sdb.antenna2()(irow)) {
+	  
+	  Float fpa(sdb.feedPa()(0));  // assumes same for all antennas!
+	  
+	  for (Int ich=0;ich<nChan;++ich) {
+	    
+	    if (!sdb.flagCube()(1,ich,irow)) {
+	      // Correct x-hands for xy-phase and add together
+	      wt0=sdb.weightSpectrum()(1,ich,irow);
+	      v=sdb.visCubeCorrected()(1,ich,irow)/Cph(ich);
+	      xf(isdb)+=Double(wt0*2.0*(fpa));
+	      yf(isdb)+=Double(wt0*real(v));
+	      wtf(isdb)+=Double(wt0);
+	    }
+	    if (!sdb.flagCube()(2,ich,irow)) {
+	      // Correct x-hands for xy-phase and add together
+	      wt0=sdb.weightSpectrum()(2,ich,irow);
+	      v=sdb.visCubeCorrected()(2,ich,irow)/conj(Cph(ich));
+	      xf(isdb)+=Double(wt0*2.0*(fpa));
+	      yf(isdb)+=Double(wt0*real(v));
+	      wtf(isdb)+=Double(wt0);
+	    }
+	  }
+	}
+      }
+    }
+    
+    // Normalize data by accumulated weights
+    for (Int isdb=0;isdb<nSDB;++isdb) {
+      if (wtf(isdb)>0.0) {
+	xf(isdb)/=wtf(isdb);
+	yf(isdb)/=wtf(isdb);
+	sigf(isdb)=sqrt(1.0/wtf(isdb));
+	maskf(isdb)=true;
+      }
+      else
+	sigf(isdb)=DBL_MAX;    // ~zero weight
+    }
+    
+    // p0=Q, p1=U, p2 = real part of net instr pol offset
+    //  x is TWICE the parallactic angle
+    CompiledFunction<AutoDiff<Double> > fn;
+    fn.setFunction("-p0*sin(x) + p1*cos(x) + p2");
+
+    LinearFit<Double> fitter;
+    fitter.setFunction(fn);
+    
+    Vector<Double> soln=fitter.fit(xf,yf,sigf,&maskf);
+
+    srcPolPar().resize(2);
+    srcPolPar()(0)=soln(0);
+    srcPolPar()(1)=soln(1);
+        
+    QU_(0,thisSpw) = soln(0);
+    QU_(1,thisSpw) = soln(1);
+
+    cout << " Fractional Poln: "
+	 << "Q = " << QU_(0,thisSpw) << ", "
+	 << "U = " << QU_(1,thisSpw) << "; "
+	 << "P = " << sqrt(soln(0)*soln(0)+soln(1)*soln(1)) << ", "
+	 << "X = " << atan2(soln(1),soln(0))*90.0/C::pi << "deg."
+	 << endl;
+    cout << " Net (over baselines) instrumental polarization: " 
+	 << soln(2) << endl;
+
+  }	
+
+}
+
+void XparangJones::globalPostSolveTinker() {
+
+    // Add QU info the the keywords
+    TableRecord& tr(ct_->rwKeywordSet());
+    Record qu;
+    qu.define("QU",QU_);
+    tr.defineRecord("QU",qu);
+
+}
+
+
+// **********************************************************
+//  XfparangJones Implementations
+//
+
+// Constructor
+XfparangJones::XfparangJones(VisSet& vs)  :
+  VisCal(vs),             // virtual base
+  VisMueller(vs),         // virtual base
+  XparangJones(vs)        // immediate parent
+{
+  if (prtlev()>2) cout << "Xfparangf::Xfparang(vs)" << endl;
+}
+
+XfparangJones::XfparangJones(String msname,Int MSnAnt,Int MSnSpw) :
+  VisCal(msname,MSnAnt,MSnSpw),             // virtual base
+  VisMueller(msname,MSnAnt,MSnSpw),         // virtual base
+  XparangJones(msname,MSnAnt,MSnSpw)        // immediate parent
+{
+  if (prtlev()>2) cout << "Xfparang::Xfparang(msname,MSnAnt,MSnSpw)" << endl;
+}
+
+XfparangJones::XfparangJones(const MSMetaInfoForCal& msmc) :
+  VisCal(msmc),             // virtual base
+  VisMueller(msmc),         // virtual base
+  XparangJones(msmc)        // immediate parent
+{
+  if (prtlev()>2) cout << "Xfparang::Xfparang(msmc)" << endl;
+}
+
+XfparangJones::XfparangJones(const Int& nAnt) :
+  VisCal(nAnt), 
+  VisMueller(nAnt),
+  XparangJones(nAnt)
+{
+  if (prtlev()>2) cout << "Xfparang::Xfparang(nAnt)" << endl;
+}
+
+XfparangJones::~XfparangJones() {
+  if (prtlev()>2) cout << "Xfparang::~Xfparang()" << endl;
+}
+
+
 
 // **********************************************************
 //  GlinXphJones Implementations
@@ -1679,6 +2365,7 @@ GlinXphfJones::GlinXphfJones(const Int& nAnt) :
 GlinXphfJones::~GlinXphfJones() {
   if (prtlev()>2) cout << "GlinXphf::~GlinXphf()" << endl;
 }
+
 
 
 
--- code/synthesis/MeasurementComponents/test/tXJones_GT.cc
+++ code/synthesis/MeasurementComponents/test/tXJones_GT.cc
@@ -294,6 +294,117 @@ TEST_F(GlinXphJonesTest, GlinXphfJonesTest){
   
   ASSERT_TRUE(allNearAbs(soldiff,0.0f,1e-7)); 
 
+}
 
+
+
+
+class XparangJonesTest : public VisCalTestBase {
+
+public:
+
+  Cube<Complex> x;
+
+  XparangJonesTest() :
+    VisCalTestBase(1,10,1,4,4,64,1,true,"lin"),
+    x(1,nChan,nAnt,Complex(0.0))
+  {
+    for (Int ich=0;ich<nChan;++ich) {
+      Float a((C::pi/4)*cos(ich*C::pi/90.0));
+      x(Slice(),Slice(ich,1,1),Slice())=Complex(cos(a),sin(a));
+    }
+
+    //summary("XparangJonesTest");
+    //cout << "x = " << phase(x)*180/C::pi << endl;
+  }
+
+
+};
+  
+
+TEST_F(XparangJonesTest, XfparangJonesTest){
+
+  // Apply-able parang
+  PJones Papp(msmc);
+  Papp.setApply();
+
+  // Apply-able Xf
+  XfJones Xapp(msmc);
+  Xapp.setApply();
+
+  for (Int ispw=0;ispw<nSpw;++ispw) { 
+    Xapp.setMeta(0,1,0.0,
+		 ispw,ssvp.freqs(ispw),
+		 nChan);
+    Xapp.sizeApplyParCurrSpw(nChan);
+    
+    Xapp.setApplyParCurrSpw(x,true,false);  // correct below will corrupt
+  }
+
+  XfparangJones XYsol(msmc);
+  Record solvePar;
+  solvePar.define("table",String("test.Xf"));
+  solvePar.define("solint",String("inf"));
+  solvePar.define("combine",String(""));
+  Vector<Int> refant(1,0); solvePar.define("refant",refant);
+  XYsol.setSolve(solvePar);
+
+  SDBList sdbs;
+  for (vi2.originChunks();vi2.moreChunks();vi2.nextChunk()) {
+    for (vi2.origin();vi2.more();vi2.next()) {
+
+      Int ispw=vb2->spectralWindows()(0);
+      Int obsid(vb2->observationId()(0));
+      Int scan(vb2->scan()(0));
+      Double timestamp(vb2->time()(0));
+      Int fldid(vb2->fieldId()(0));
+      Vector<Double> freqs(vb2->getFrequencies(0));
+      Vector<Int> a1(vb2->antenna1());
+      Vector<Int> a2(vb2->antenna2());
+
+      vb2->resetWeightsUsingSigma();
+
+      Cube<Complex> vC(vb2->visCube());
+      vb2->setVisCubeCorrected(vC);
+      vb2->setFlagCube(vb2->flagCube());
+
+      Xapp.setMeta(obsid,scan,timestamp,
+		   ispw,freqs,
+		   fldid);
+      Xapp.correct2(*vb2,false,false,false);  // (trial?,doWtSp?,dosync?)
+
+
+      //cout << "vCC=" << Vector<Complex>(vb2->visCubeCorrected()(Slice(),Slice(nChan/2),Slice(0))) << endl;
+      //cout << "vCM=" << Vector<Complex>(vb2->visCubeModel()(Slice(),Slice(nChan/2),Slice(0))) << endl;
+
+
+      sdbs.add(*vb2);
+      //cout << "sdb.feedPa() = " << sdbs(sdbs.nSDB()-1).feedPa() << endl;
+    }
+  }
+
+  // Setup meta & sizes for the solve
+  XYsol.setMeta(sdbs.aggregateObsId(),
+		sdbs.aggregateScan(),
+		sdbs.aggregateTime(),
+		sdbs.aggregateSpw(),
+		sdbs.freqs(),
+		sdbs.aggregateFld());
+  XYsol.sizeSolveParCurrSpw(sdbs.nChannels()); 
+
+  // Call the specialized solver
+  XYsol.selfSolveOne(sdbs);
+
+  //cout << "srcPolPar() = " << real(XYsol.srcPolPar()) << endl;
+  ASSERT_NEAR(0.04,real(XYsol.srcPolPar()(0)),1e-3);
+  ASSERT_NEAR(0.03,real(XYsol.srcPolPar()(1)),1e-3);
+  
+  Cube<Complex> soln(XYsol.solveAllCPar()(Slice(0),Slice(),Slice()));
+  //cout << "soln = " << phase(soln.xyPlane(0))*180/C::pi << endl;
+
+  Cube<Float> soldiff=amplitude(soln-x);
+  //cout << "Diff = " << soldiff  << endl;
+  
+  ASSERT_TRUE(allNearAbs(soldiff,0.0f,1e-7)); 
 
 }
