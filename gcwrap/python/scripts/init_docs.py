78a81c017343f422018f70818930e4172620397d
Darrell Schiebel <darrell@schiebel.us>
2018-12-18 16:52:23 -0500

    make details private

--- gcwrap/python/scripts/init_docs.py
+++ gcwrap/python/scripts/init_docs.py
@@ -9,41 +9,41 @@ class __doc(object):
 
         version = "%d.%d.%d" % tuple(cu.version( )[:3])
 
-        self.task_url = "https://casa.nrao.edu/PloneResource/casa-" + version + "/taskXml/"
-        self.tool_url = "https://casa.nrao.edu/PloneResource/casa-" + version + "/toolXml/"
-        self.toc_url = "https://casa.nrao.edu/casadocs/casa-" + version + "/global-task-list"
-        self.start_url = "https://casa.nrao.edu/casadocs/casa-" + version
+        self.__task_url = "https://casa.nrao.edu/PloneResource/casa-" + version + "/taskXml/"
+        self.__tool_url = "https://casa.nrao.edu/PloneResource/casa-" + version + "/toolXml/"
+        self.__toc_url = "https://casa.nrao.edu/casadocs/casa-" + version + "/global-task-list"
+        self.__start_url = "https://casa.nrao.edu/casadocs/casa-" + version
 
-        self.task_prefix = "https://casa.nrao.edu/casadocs/casa-" + version + "/global-task-list/task_"
-        self.tool_prefix = "https://casa.nrao.edu/casadocs/casa-" + version + "/global-tool-list/tool_"
+        self.__task_prefix = "https://casa.nrao.edu/casadocs/casa-" + version + "/global-task-list/task_"
+        self.__tool_prefix = "https://casa.nrao.edu/casadocs/casa-" + version + "/global-tool-list/tool_"
 
-        self.tasklist = [ ]
-        self.toollist = [ ]
+        self.__task_list = [ ]
+        self.__tool_list = [ ]
 
     def __call__( self, topic=None ):
         "open browser with documentation, try \"doc('toc')\""
 
-        if len(self.tasklist) == 0:
+        if len(self.__task_list) == 0:
             try:
-                self.tasklist = re.findall("\w+.xml", urllib2.urlopen(self.task_url).read().decode())
+                self.__task_list = re.findall("\w+.xml", urllib2.urlopen(self.__task_url).read().decode())
             except:
-                self.tasklist = [ ]
+                self.__task_list = [ ]
 
-        if len(self.toollist) == 0:
+        if len(self.__tool_list) == 0:
             try:
-                self.toollist = re.findall("\w+.xml", urllib2.urlopen(self.tool_url).read().decode())
+                self.__tool_list = re.findall("\w+.xml", urllib2.urlopen(self.__tool_url).read().decode())
             except:
-                self.toollist = [ ]
+                self.__tool_list = [ ]
 
         if type(topic) != str or topic == "toc":
-            webbrowser.open_new_tab(self.toc_url)
+            webbrowser.open_new_tab(self.__toc_url)
         elif topic == "start":
-            webbrowser.open_new_tab(self.start_url)
-        elif topic+'.xml' in self.tasklist:
-            webbrowser.open_new_tab(self.task_prefix+topic+"/parameters")
-        elif topic+'.xml' in self.toollist:
-            webbrowser.open_new_tab(self.tool_prefix+topic+"/methods")
+            webbrowser.open_new_tab(self.__start_url)
+        elif topic+'.xml' in self.__task_list:
+            webbrowser.open_new_tab(self.__task_prefix+topic+"/parameters")
+        elif topic+'.xml' in self.__tool_list:
+            webbrowser.open_new_tab(self.__tool_prefix+topic+"/methods")
         else:
-            webbrowser.open_new_tab(self.toc_url)
+            webbrowser.open_new_tab(self.__toc_url)
 
 doc = __doc( )
